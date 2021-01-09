9b96de85c4643728b3f79db29439b332bb123e3a
kevin <kevin@kevin-PC>
2021-01-09 20:49:01 +0800

    更新脚本

--- autobuild.sh
+++ autobuild.sh
@@ -1,8 +1,9 @@
 yum install git maven -y && \
 git clone https://gitee.com/KevinHuangwl/northstar.git && \
 curl -sSL https://get.daocloud.io/docker | sh && \
-echo {"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]} >/etc/docker/daemon.json && \
 systemctl start docker && \
+echo {"registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]} >/etc/docker/daemon.json && \
+systemctl restart docker && \
 docker pull mongo:4.0 && \
 mvn clean install && docker build -t northstar-trader . && \
 echo “安装完成”
