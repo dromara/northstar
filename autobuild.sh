490b480ee29ada24b3c52ca147f4a72286797130
kevin <kevin@kevin-PC>
2021-01-09 21:57:58 +0800

    更新

--- autobuild.sh
+++ autobuild.sh
@@ -3,6 +3,8 @@ rm -rf northstar
 git clone https://gitee.com/KevinHuangwl/northstar.git
 curl -sSL https://get.daocloud.io/docker | sh 
 systemctl start docker 
+echo {\"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn\"]} >/etc/docker/daemon.json
+systemctl restart docker
 docker pull mongo:4.0 
 cd northstar
 mvn clean install && docker build -t northstar-trader . 
