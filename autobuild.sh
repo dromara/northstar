6f9a9a8dc040ea05e78cb3273b1807229a19e1de
kevin <kevin@kevin-PC>
2021-01-10 13:30:35 +0800

    更新脚本

--- autobuild.sh
+++ autobuild.sh
@@ -1,4 +1,4 @@
-yum install git java-11 nodejs -y
+yum install git java-11 nodejs wget -y
 cd ~
 wget https://mirror.bit.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar -xvf apache-maven-3.6.3-bin.tar.gz
@@ -11,7 +11,7 @@ systemctl restart docker
 docker pull mongo:4.0 
 docker pull nginx
 cd ~/northstar
-\cp -f settings.xml /etc/maven/settings.xml
+\cp -f settings.xml ~/apache-maven-3.6.3/conf/settings.xml
 ~/apache-maven-3.6.3/bin/mvn clean install
 docker build -t northstar-trader . 
 cd ~/northstar-monitor
