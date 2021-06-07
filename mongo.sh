828fd1682865ea6f946043b26fa28b34f441eb67
KevinHuangwl <kevinbauer@163.com>
2021-06-07 19:53:26 +0800

    更新

--- mongo.sh
+++ mongo.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 echo "安装MongoDB"
-curl -sSL https://get.daocloud.io/docker | sh 
-systemctl enable docker
-systemctl start docker 
-echo {\"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn\"]} >/etc/docker/daemon.json
-systemctl restart docker
-docker pull mongo:4.0 
-docker run --name Mongo --net=host -d mongo:4.0
-echo "docker start Mongo" >>/etc/rc.local
+mv mongo.repo /etc/yum.repos.d/mongodb-org-4.0.repo
+yum install -y mongodb-org
+systemctl start mongod
 echo “安装完成”
