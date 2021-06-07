b6679fd5a8434e8097263c0547d656284f9e4d3a
KevinHuangwl <kevinbauer@163.com>
2021-06-07 19:21:49 +0800

    更新

--- mongo.sh
+++ mongo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "安装MongoDB"
-sudo yum install libcurl openssl -y
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1604-4.2.14.tgz
-tar -xvzf mongodb-linux-x86_64-ubuntu1604-4.2.14.tgz
-rm -f mongodb-linux-x86_64-ubuntu1604-4.2.14.tgz
-mv mongodb-linux-x86_64-ubuntu1604-4.2.14 ~/mongodb
-ln -s  ~/mongodb/bin/* /usr/local/bin/
-mkdir -p /var/lib/mongo
-mkdir -p /var/log/mongodb
-chown -R mongod:mongod /var/lib/mongo /var/log/mongodb
-mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log --fork
+curl -sSL https://get.daocloud.io/docker | sh 
+systemctl enable docker
+systemctl start docker 
+echo {\"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn\"]} >/etc/docker/daemon.json
+systemctl restart docker
+docker pull mongo:4.0 
+docker run --name Mongo --net=host -d mongo:4.0
+echo "docker start Mongo" >>/etc/rc.local
+echo “安装完成”
