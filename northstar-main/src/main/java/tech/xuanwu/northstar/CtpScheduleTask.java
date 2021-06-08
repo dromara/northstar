2724cf14c432ab2354235e53b65e0f823653d057
KevinHuangwl <kevinbauer@163.com>
2021-06-08 22:36:35 +0800

    修复定时器不工作问题

--- northstar-main/src/main/java/tech/xuanwu/northstar/CtpScheduleTask.java
+++ northstar-main/src/main/java/tech/xuanwu/northstar/CtpScheduleTask.java
@@ -34,7 +34,7 @@ public class CtpScheduleTask {
 	@Autowired
 	private BarBufferManager bbMgr;
 
-	@Scheduled(cron="0 0/1 0-1,9-14,21-23 ? * 1/5")
+	@Scheduled(cron="0 0/1 0-1,9-14,21-23 ? * 1-5")
 	public void timelyCheckConnection() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			return;
@@ -43,7 +43,7 @@ public class CtpScheduleTask {
 		log.info("开盘时间连线巡检");
 	}
 	
-	@Scheduled(cron="0 55 8,20 ? * 1/5")
+	@Scheduled(cron="0 55 8,20 ? * 1-5")
 	public void dailyCheckConnection() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			log.info("当前为假期，不进行连线");
@@ -64,7 +64,7 @@ public class CtpScheduleTask {
 		}
 	}
 	
-	@Scheduled(cron="58 0 9,21 ? * 1/5")
+	@Scheduled(cron="58 0 9,21 ? * 1-5")
 	public void dataCleaning() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			return;
@@ -99,7 +99,7 @@ public class CtpScheduleTask {
 	 * 正常来讲，开盘第一分钟的bar数据会在至少一分钟后才会生成成bar并提交到BarBufferManager，
 	 * 那么，如果未满足一分钟却有数据在buffer中，那么这些数据肯定是垃圾数据
 	 */
-	@Scheduled(cron="55 0/1 0-2,9-14,21-23 ? * 1/5")
+	@Scheduled(cron="55 0/1 0-2,9-14,21-23 ? * 1-5")
 	public void timelySaveBar() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			return;
@@ -111,7 +111,7 @@ public class CtpScheduleTask {
 	/**
 	 * 收盘时间定时持久化
 	 */
-	@Scheduled(cron="10 0 3,15 ? * 1/5")
+	@Scheduled(cron="10 0 3,15 ? * 1-5")
 	public void saveBarAfterMarketClose() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			return;
@@ -120,12 +120,12 @@ public class CtpScheduleTask {
 		log.info("收盘时间定时持久化Bar数据任务");
 	}
 	
-	@Scheduled(cron="0 1 15 ? * 1/5")
+	@Scheduled(cron="0 1 15 ? * 1-5")
 	public void dailySettlement() {
 		if(holidayMgr.isHoliday(LocalDate.now())) {
 			return;
 		}
 		log.info("日结算定时任务");
 	}
-	
+
 }
