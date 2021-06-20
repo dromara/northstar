1f82cff9dc50e05a84103d0d6a0b4d6a294d8c77
KevinHuangwl <kevinbauer@163.com>
2021-06-20 23:49:12 +0800

    更新脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~/northstar
-~/apache-maven-3.6.3/bin/mvn clean install
+~/apache-maven-3.6.3/bin/mvn clean install -Dmaven.test.skip=true
 \mv -f northstar-main/target/northstar-main*.jar ~/northstar.jar
 
