6b9d9c328c7f605c778201eafb941cbfe9d596bc
KevinHuangwl <kevinbauer@163.com>
2021-07-13 20:58:00 +0800

    #I40ILW 增加订单与成交的邮件通知功能

--- northstar-plugin-notification/src/main/java/tech/xuanwu/northstar/plugin/mail/NotificationDispatcher.java
+++ northstar-plugin-notification/src/main/java/tech/xuanwu/northstar/plugin/mail/NotificationDispatcher.java
@@ -13,7 +13,10 @@ import tech.xuanwu.northstar.common.event.GenericEventHandler;
 import tech.xuanwu.northstar.common.event.NorthstarEvent;
 import tech.xuanwu.northstar.common.event.NorthstarEventType;
 import tech.xuanwu.northstar.common.event.PluginEventBus;
+import xyz.redtorch.pb.CoreEnum.OrderStatusEnum;
 import xyz.redtorch.pb.CoreField.NoticeField;
+import xyz.redtorch.pb.CoreField.OrderField;
+import xyz.redtorch.pb.CoreField.TradeField;
 
 @Component
 public class NotificationDispatcher extends AbstractEventHandler implements InitializingBean, GenericEventHandler{
@@ -31,6 +34,19 @@ public class NotificationDispatcher extends AbstractEventHandler implements Init
 			add(NorthstarEventType.NOTICE);
 			add(NorthstarEventType.CONNECTED);
 			add(NorthstarEventType.DISCONNECTED);
+			add(NorthstarEventType.TRADE);
+			add(NorthstarEventType.ORDER);
+		}
+	};
+	
+	private Set<OrderStatusEnum> interestedOrderStatus = new HashSet<>() {
+		private static final long serialVersionUID = 1L;
+
+		{
+			add(OrderStatusEnum.OS_AllTraded);
+			add(OrderStatusEnum.OS_Canceled);
+			add(OrderStatusEnum.OS_Rejected);
+			add(OrderStatusEnum.OS_Touched);
 		}
 	};
 	
@@ -58,6 +74,12 @@ public class NotificationDispatcher extends AbstractEventHandler implements Init
 		case DISCONNECTED:
 			handleDisconnection((String) e.getData());
 			break;
+		case ORDER:
+			handleOrder((OrderField) e.getData());
+			break;
+		case TRADE:
+			handleTrade((TradeField) e.getData());
+			break;
 		default:
 			throw new IllegalArgumentException("未定义处理类型：" + e.getEvent());
 		}
@@ -76,6 +98,26 @@ public class NotificationDispatcher extends AbstractEventHandler implements Init
 	
 	private void handleDisconnection(String gatewayId) {
 		Message msg = new Message(String.format("[%s] - 离线", gatewayId), "");
+		doSend(msg);
+	}
+	
+	private void handleOrder(OrderField order) {
+		String gatewayId = order.getGatewayId();
+		//避免订单状态变化导致邮箱过多，过滤掉不感兴趣的非关键状态
+		if(!interestedOrderStatus.contains(order.getOrderStatus())) {
+			return;
+		}
+		Message msg = new Message(String.format("[%s] - 订单：%s，合约：%s %d手", 
+				gatewayId, order.getOrderStatus(), order.getContract().getName(), order.getTotalVolume()), 
+				String.format("合约：%s\n手数：%d\n已成交：%d\n价钱：%0.2f", 
+						order.getContract().getName(), order.getTotalVolume(), order.getTradedVolume(), order.getPrice()));
+		doSend(msg);
+	}
+	
+	private void handleTrade(TradeField trade) {
+		String gatewayId = trade.getGatewayId();
+		Message msg = new Message(String.format("[%s] - 成交合约：%s %d手", gatewayId, trade.getContract().getName(), trade.getVolume()),
+				"");
 		doSend(msg);
 	}
 	
