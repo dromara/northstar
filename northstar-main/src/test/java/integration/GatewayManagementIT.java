76829d57f22aede27e6c0b0f7cd356c5b1098fe6
KevinHuangwl <kevinbauer@163.com>
2021-08-21 19:21:38 +0800

    #I46N7P 搭建自动化流水线框架

---
+++ northstar-main/src/test/java/integration/GatewayManagementIT.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author kevin
+ *
+ */
+public class GatewayManagementIT {
+
+	@Test
+	public void test() {
+	}
+
+}
