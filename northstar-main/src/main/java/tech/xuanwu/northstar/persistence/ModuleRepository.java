9603330f1fec4d77257c728f45798b862d171cd3
KevinHuangwl <kevinbauer@163.com>
2021-08-29 10:54:24 +0800

    修复持久化方法

--- northstar-main/src/main/java/tech/xuanwu/northstar/persistence/ModuleRepository.java
+++ northstar-main/src/main/java/tech/xuanwu/northstar/persistence/ModuleRepository.java
@@ -1,8 +1,6 @@
 package tech.xuanwu.northstar.persistence;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -11,14 +9,16 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 import tech.xuanwu.northstar.strategy.common.model.ModuleInfo;
-import tech.xuanwu.northstar.strategy.common.model.ModuleStatus;
-import tech.xuanwu.northstar.strategy.common.model.TradeDescription;
+import tech.xuanwu.northstar.strategy.common.model.entity.ModuleStatusEntity;
+import tech.xuanwu.northstar.strategy.common.model.entity.TradeDescriptionEntity;
 
 @Repository
 public class ModuleRepository {
 
 	@Autowired
 	private MongoTemplate mongo;
+	
+	private static final String MODULE_NAME = "moduleName";
 	
 	/*************/
 	/**	模组信息	**/
@@ -37,42 +37,37 @@ public class ModuleRepository {
 	}
 	
 	public void deleteModuleInfoById(String moduleId) {
-		mongo.remove(Query.query(Criteria.where("moduleName").is(moduleId)), ModuleInfo.class);
+		mongo.remove(Query.query(Criteria.where(MODULE_NAME).is(moduleId)), ModuleInfo.class);
 	}
 	
 	/*************/
 	/**	模组状态	**/
 	/*************/
-	public Map<String, ModuleStatus> loadModuleStatus(){
-		List<ModuleStatus> statusList = mongo.findAll(ModuleStatus.class);
-		Map<String, ModuleStatus> statusMap = new HashMap<>(statusList.size());
-		for(ModuleStatus ms : statusList) {
-			statusMap.put(ms.getModuleName(), ms);
-		}
-		return statusMap;
+	public ModuleStatusEntity loadModuleStatus(String moduleName){
+		return mongo.findOne(Query.query(Criteria.where(MODULE_NAME).is(moduleName)), ModuleStatusEntity.class);
 	}
 	
-	public void saveModuleStatus(ModuleStatus status) {
+	public void saveModuleStatus(ModuleStatusEntity status) {
 		mongo.save(status);
 	}
 	
 	public void removeModuleStatus(String moduleName) {
-		mongo.remove(Query.query(Criteria.where("moduleName").is(moduleName)), ModuleStatus.class);
+		mongo.remove(Query.query(Criteria.where(MODULE_NAME).is(moduleName)), ModuleStatusEntity.class);
 	}
 	
 	/*************/
 	/**	模组成交	**/
 	/*************/
-	public void saveTradeDescription(TradeDescription tradeDescription) {
+	public void saveTradeDescription(TradeDescriptionEntity tradeDescription) {
 		mongo.save(tradeDescription);
 	}
 	
-	public List<TradeDescription> findTradeDescription(String moduleName) {
-		return mongo.find(Query.query(Criteria.where("moduleName").is(moduleName)), TradeDescription.class);
+	public List<TradeDescriptionEntity> findTradeDescription(String moduleName) {
+		return mongo.find(Query.query(Criteria.where(MODULE_NAME).is(moduleName)), TradeDescriptionEntity.class);
 	}
 	
 	public void removeTradeDescription(String moduleName) {
-		mongo.remove(Query.query(Criteria.where("moduleName").is(moduleName)), TradeDescription.class);
+		mongo.remove(Query.query(Criteria.where(MODULE_NAME).is(moduleName)), TradeDescriptionEntity.class);
 	}
 	
 }
