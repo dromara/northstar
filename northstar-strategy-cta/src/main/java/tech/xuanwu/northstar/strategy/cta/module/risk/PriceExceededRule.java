9a468c71a86bae0f1185e882848807cdc3d86565
KevinHuangwl <kevinbauer@163.com>
2021-09-26 00:16:18 +0800

    #I4BRKU 修复价格限制风控策略可能对市价单造成误判问题

--- northstar-strategy-cta/src/main/java/tech/xuanwu/northstar/strategy/cta/module/risk/PriceExceededRule.java
+++ northstar-strategy-cta/src/main/java/tech/xuanwu/northstar/strategy/cta/module/risk/PriceExceededRule.java
@@ -13,6 +13,7 @@ import tech.xuanwu.northstar.strategy.common.constants.RiskAuditResult;
 import tech.xuanwu.northstar.strategy.common.model.ModuleStatus;
 import tech.xuanwu.northstar.strategy.common.model.meta.DynamicParams;
 import xyz.redtorch.pb.CoreEnum.DirectionEnum;
+import xyz.redtorch.pb.CoreEnum.OrderPriceTypeEnum;
 import xyz.redtorch.pb.CoreField.SubmitOrderReqField;
 import xyz.redtorch.pb.CoreField.TickField;
 
@@ -34,7 +35,7 @@ public class PriceExceededRule implements RiskControlRule, DynamicParamsAware{
 	public short canDeal(TickField tick, ModuleStatus moduleStatus) {
 		Assert.isTrue(StringUtils.equals(tick.getUnifiedSymbol(), orderReq.getContract().getUnifiedSymbol()), "行情合约与订单不一致");
 		int factor = orderReq.getDirection() == DirectionEnum.D_Buy ? 1 : -1;
-		if(factor * (tick.getLastPrice() - orderReq.getPrice()) > priceDifTolerance) {
+		if(orderReq.getOrderPriceType() == OrderPriceTypeEnum.OPT_LimitPrice && factor * (tick.getLastPrice() - orderReq.getPrice()) > priceDifTolerance) {
 			log.info("委托超价限制：限制为{}，期望价为{}，实际价为{}", priceDifTolerance, orderReq.getPrice(), tick.getLastPrice());
 			return RiskAuditResult.REJECTED;
 		}
