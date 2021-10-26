0be161bdea8a20910b5840b93e00639acb69ab6e
KevinHuangwl <kevinbauer@163.com>
2021-10-26 22:27:40 +0800

    更新环境安装脚本

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ echo "准备环境依赖..."
 yum install git java-11-openjdk-devel.x86_64 nodejs wget -y
 cd ~
 echo "安装Maven"
-wget https://mirrors.bfsu.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+wget --no-check-certificate https://mirrors.bfsu.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 tar -xvf apache-maven-3.6.3-bin.tar.gz
 ln -s ~/apache-maven-3.6.3/bin/mvn /usr/local/bin/
 wget https://gitee.com/dromara/northstar/raw/master/settings.xml
