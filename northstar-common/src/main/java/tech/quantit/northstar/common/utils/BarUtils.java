66b28ad5af99dc36d85a26eb4d78685bf2e032db
KevinHuangwl <kevinbauer@163.com>
2021-12-26 18:37:36 +0800

    增加BAR工具类用于N周期的BAR数据合成

---
+++ northstar-common/src/main/java/tech/quantit/northstar/common/utils/BarUtils.java
@@ -0,0 +1,38 @@
+package tech.quantit.northstar.common.utils;
+
+import java.util.List;
+
+import xyz.redtorch.pb.CoreField.BarField;
+
+public interface BarUtils {
+
+	static BarField merge(List<BarField> bars) {
+		if(bars.isEmpty()) {
+			throw new IllegalArgumentException("空BAR集合无法合成新BAR");
+		}
+		BarField lastBar = bars.get(bars.size() - 1);
+		BarField firstBar = bars.get(0);
+		double open = firstBar.getOpenPrice();
+		double close = bars.get(bars.size() - 1).getClosePrice();
+		double high = bars.stream().mapToDouble(BarField::getHighPrice).max().getAsDouble();
+		double low = bars.stream().mapToDouble(BarField::getLowPrice).min().getAsDouble();
+		long vol = bars.stream().mapToLong(BarField::getVolumeDelta).sum();
+		double openInterest = bars.stream().mapToDouble(BarField::getOpenInterestDelta).sum();
+		long numOfTrade = bars.stream().mapToLong(BarField::getNumTradesDelta).sum();
+		double turnover = bars.stream().mapToDouble(BarField::getTurnoverDelta).sum();
+		return BarField.newBuilder(firstBar)
+				.setClosePrice(close)
+				.setOpenPrice(open)
+				.setHighPrice(high)
+				.setLowPrice(low)
+				.setVolume(lastBar.getVolume())
+				.setVolumeDelta(vol)
+				.setNumTrades(lastBar.getNumTrades())
+				.setNumTradesDelta(numOfTrade)
+				.setTurnover(lastBar.getTurnover())
+				.setTurnoverDelta(turnover)
+				.setOpenInterest(lastBar.getOpenInterest())
+				.setOpenInterestDelta(openInterest)
+				.build();
+	}
+}
