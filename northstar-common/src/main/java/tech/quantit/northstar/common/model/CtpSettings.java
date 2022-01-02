199323990344e65a627e606ed478b7e9851ed3d2
KevinHuangwl <kevinbauer@163.com>
2022-01-02 08:35:30 +0800

    优化CTP账户配置

--- northstar-common/src/main/java/tech/quantit/northstar/common/model/CtpSettings.java
+++ northstar-common/src/main/java/tech/quantit/northstar/common/model/CtpSettings.java
@@ -14,11 +14,4 @@ public class CtpSettings implements GatewaySettings{
 	private String userId;
 	private String password;
 	private String brokerId;
-	private String tdHost;
-	private String tdPort;
-	private String mdHost;
-	private String mdPort;
-	private String authCode;
-	private String userProductInfo; 
-	private String appId;
 }
