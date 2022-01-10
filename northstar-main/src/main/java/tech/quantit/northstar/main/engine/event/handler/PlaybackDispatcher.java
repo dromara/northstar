e6b9b2db00b992f16edf5dafe7ead91c914f441b
KevinHuangwl <kevinbauer@163.com>
2022-01-10 23:31:43 +0800

    修复模组回测的过滤事件，解决被实盘行情干扰问题

--- northstar-main/src/main/java/tech/quantit/northstar/main/engine/event/handler/PlaybackDispatcher.java
+++ northstar-main/src/main/java/tech/quantit/northstar/main/engine/event/handler/PlaybackDispatcher.java
@@ -1,13 +1,9 @@
 package tech.quantit.northstar.main.engine.event.handler;
 
-import tech.quantit.northstar.common.constant.Constants;
 import tech.quantit.northstar.common.event.FastEventEngine.NorthstarEventDispatcher;
 import tech.quantit.northstar.common.event.NorthstarEvent;
+import tech.quantit.northstar.common.event.NorthstarEventType;
 import tech.quantit.northstar.common.event.PlaybackEventBus;
-import xyz.redtorch.pb.CoreField.AccountField;
-import xyz.redtorch.pb.CoreField.OrderField;
-import xyz.redtorch.pb.CoreField.PositionField;
-import xyz.redtorch.pb.CoreField.TradeField;
 
 public class PlaybackDispatcher implements NorthstarEventDispatcher{
 	
@@ -19,11 +15,7 @@ public class PlaybackDispatcher implements NorthstarEventDispatcher{
 
 	@Override
 	public void onEvent(NorthstarEvent event, long sequence, boolean endOfBatch) throws Exception {
-		boolean isPlaybackAccount = event.getData() instanceof AccountField account && account.getGatewayId().contains(Constants.PLAYBACK_GATEWAY);
-		boolean isPlaybackOrder = event.getData() instanceof OrderField order && order.getGatewayId().contains(Constants.PLAYBACK_GATEWAY);
-		boolean isPlaybackTrade = event.getData() instanceof TradeField trade && trade.getGatewayId().contains(Constants.PLAYBACK_GATEWAY);
-		boolean isPlaybackPosition = event.getData() instanceof PositionField position && position.getGatewayId().contains(Constants.PLAYBACK_GATEWAY);
-		if(isPlaybackAccount || isPlaybackOrder || isPlaybackTrade || isPlaybackPosition) {
+		if(event.getEvent() != NorthstarEventType.TICK && event.getEvent() != NorthstarEventType.BAR) {
 			eventBus.post(event);
 		}
 	}
