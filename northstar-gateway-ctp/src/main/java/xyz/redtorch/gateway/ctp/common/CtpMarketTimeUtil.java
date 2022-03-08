014068e530d6d024cb392f1ec2d07b3526d5742b
KevinHuangwl <kevinbauer@163.com>
2022-03-08 23:56:00 +0800

    修复行情数据状态判断漏洞

--- northstar-gateway-ctp/src/main/java/xyz/redtorch/gateway/ctp/common/CtpMarketTimeUtil.java
+++ northstar-gateway-ctp/src/main/java/xyz/redtorch/gateway/ctp/common/CtpMarketTimeUtil.java
@@ -1,6 +1,7 @@
 package xyz.redtorch.gateway.ctp.common;
 
 import java.time.LocalTime;
+import java.util.regex.Pattern;
 
 import tech.quantit.northstar.common.constant.TickType;
 import tech.quantit.northstar.common.utils.MarketTimeUtil;
@@ -17,49 +18,42 @@ public class CtpMarketTimeUtil implements MarketTimeUtil{
 	final long nightMarketEndTime = LocalTime.of(2, 30, 0, 999999).toNanoOfDay();
 	final long dayMarketStartTime = LocalTime.of(8, 58, 59, 999999999).toNanoOfDay();
 	final long dayMarketOpenTime = LocalTime.of(8, 59, 59, 999999999).toNanoOfDay();
-	final long nightMarketClosingTime1 = LocalTime.of(23, 0, 0).toNanoOfDay();
-	final long nightMarketClosingTime2 = LocalTime.of(1, 0, 0).toNanoOfDay();
-	final long nightMarketClosingTime3 = LocalTime.of(2, 30, 0).toNanoOfDay();
-	final long dayMarketClosingTime1 = LocalTime.of(11, 30, 0).toNanoOfDay();
-	final long dayMarketClosingTime2 = LocalTime.of(15, 0, 0).toNanoOfDay();
-	final long dayMarketClosingTime3 = LocalTime.of(15, 15, 0).toNanoOfDay();
-	final long dayMarketEndTime = LocalTime.of(15, 15, 0, 999999).toNanoOfDay();
-	
-	final long[] closingArr = new long[] {
-			nightMarketClosingTime1, 
-			nightMarketClosingTime2, 
-			nightMarketClosingTime3, 
-			dayMarketClosingTime1, 
-			dayMarketClosingTime2, 
-			dayMarketClosingTime3};
+	final long dayMarketEndTime = LocalTime.of(15, 0, 0, 999999).toNanoOfDay();
+	final long dayMarketOpenTime2 = LocalTime.of(9, 29, 59, 999999999).toNanoOfDay();
+	final long dayMarketEndTime2 = LocalTime.of(15, 15, 0, 999999).toNanoOfDay();
 	
 	private static final long LESS_THEN_HALF_SEC_IN_NANO = 1000000000L;
+	
+	private Pattern bondPtn = Pattern.compile("^T[SF]?[0-9]{4}$");
+	
 	/**
 	 * 根据时间判定Tick类型
 	 */
 	@Override
-	public TickType resolveTickType(LocalTime time) {
+	public TickType resolveTickType(String symbol, LocalTime time) {
+		boolean isBond = bondPtn.matcher(symbol).matches();
 		long curTime = time.toNanoOfDay();
-		if(curTime < nightMarketEndTime || curTime > dayMarketOpenTime && curTime < dayMarketEndTime || curTime > nightMarketOpenTime) {
-			if(aroundAny(closingArr, curTime)) {
-				return TickType.CLOSING_TICK;
+		
+		if(isBond) {
+			if(curTime > dayMarketOpenTime2 && curTime < dayMarketEndTime2) {
+					return closeToTime(dayMarketEndTime2, curTime) ? TickType.CLOSING_TICK : TickType.NORMAL_TICK;
 			}
-			return TickType.NORMAL_TICK;
-		}
-		if(curTime > nightMarketStartTime && curTime < nightMarketOpenTime
-				|| curTime > dayMarketStartTime && curTime < dayMarketOpenTime) {
-			return TickType.PRE_OPENING_TICK;
+			if(curTime > dayMarketStartTime && curTime < dayMarketOpenTime2) {
+				return TickType.PRE_OPENING_TICK;
+			}
+		} else {
+			if(curTime < nightMarketEndTime || (curTime > dayMarketOpenTime && curTime < dayMarketEndTime) || curTime > nightMarketOpenTime) {
+				return closeToTime(dayMarketEndTime, curTime) ? TickType.CLOSING_TICK : TickType.NORMAL_TICK;
+			}
+			if(curTime > nightMarketStartTime && curTime < nightMarketOpenTime
+					|| curTime > dayMarketStartTime && curTime < dayMarketOpenTime) {
+				return TickType.PRE_OPENING_TICK;
+			}
 		}
 		return TickType.NON_OPENING_TICK;
 	}
 	
-	public boolean aroundAny(long[] baseTimeList, long time) {
-		for(long baseTime : baseTimeList) {
-			if(Math.abs(time - baseTime) <= LESS_THEN_HALF_SEC_IN_NANO) {
-				return true;
-			}
-		}
-		return false;
+	public boolean closeToTime(long baseTime, long time) {
+		return time < baseTime && baseTime - time < LESS_THEN_HALF_SEC_IN_NANO;
 	}
-
 }
