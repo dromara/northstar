729c233c51bb771e44913b83c73d78200adf886d
KevinHuangwl <kevinbauer@163.com>
2022-03-19 14:30:24 +0800

    更新启动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,23 +7,6 @@ if [[ `pgrep -a java | grep northstar.jar | wc -l` > 0 ]]; then
 	kill `pgrep -a java | grep northstar.jar | awk '{print $1}'`
 fi
 
-# 停止前端进程
-if [[ `pgrep node | wc -l` > 0 ]]; then
-	kill `pgrep node`
-fi
-
-# 移除原有目录
-if [[ -n `ls ~ | grep northstar-monitor` ]]; then
-	rm -rf ~/northstar-monitor
-	rm -f dist.tar.gz
-fi
-
-# 不同的版本对应的前端部署包可能不同
-cd ~ && wget https://gitee.com/dromara/northstar-monitor/attach_files/990941/download/dist.tar.gz
-tar -xvf dist.tar.gz && rm -f dist.tar.gz
-mkdir ~/northstar-monitor
-mv dist ~/northstar-monitor/dist
-
 cd ~/northstar
 ~/apache-maven-3.6.3/bin/mvn clean install -Dmaven.test.skip=true
 \mv -f northstar-main/target/northstar-main*.jar ~/northstar.jar
