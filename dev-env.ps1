95f1b8eb9f15b2918c1a402b037aefc59384e40e
KevinHuangwl <kevinbauer@163.com>
2022-04-16 09:19:36 +0800

    更新环境脚本

---
+++ dev-env.ps1
@@ -0,0 +1,11 @@
+
+#检查env.ps1脚本是否已经运行过，没有的话就运行，确保环境准备好
+
+#Git下载地址
+$GitDownloadUrl = "https://github.com/git-for-windows/git/releases/download/v2.35.1.windows.2/Git-2.35.1.2-64-bit.exe"
+#Eclipse地址
+
+#northstar仓库地址
+
+
+#northstar-external仓库地址
