3591d5c6cba1c29660edc14ecfcceafdae801da6
KevinHuangwl <kevinbauer@163.com>
2022-04-16 10:45:50 +0800

    更新脚本

--- env.ps1
+++ env.ps1
@@ -11,7 +11,6 @@ If(!(test-path $BasePath))
 {
    New-Item -Path $BasePath -ItemType Directory
 }
-
 #JDK17下载地址
 $JDK17DownloadUrl = "https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.msi"
 #Node14下载地址
@@ -32,7 +31,7 @@ function downloadAndInstallMSI([string] $url, [string] $destPath, [string] $file
 	"Start download $fileName"
 	Invoke-WebRequest -Uri $url -OutFile "$destPath$fileName"
 	"Start install $fileName"
-	msiexec.exe /log "$fileName.txt" /i "$destPath$fileName" /qr $args
+	msiexec.exe /log "log_$fileName" /i "$destPath$fileName" /qr $args
 	"$fileName installed"
 }
 
@@ -60,6 +59,10 @@ If(checkExist java.exe 17*){
     "JDK17 installed"
 } else {
 	downloadAndInstallMSI $JDK17DownloadUrl $BasePath jdk-17_windows-x64_bin.msi
+	$programPath = "C:\Program Files\Java"
+	$jdkPath = getInstallPath $programPath jdk-17*
+	$path = "$jdkPath\bin;" + [Environment]::getEnvironmentVariable("Path")
+	setEnvPath Path $path
 }
 
 #Node14环境安装
@@ -67,13 +70,16 @@ If(checkExist node.exe 14*){
 	"Node14 installed"
 } else {
 	downloadAndInstallMSI $Node14DownloadUrl $BasePath node-v14.19.0-x64.msi
+	$programPath = ""
+	
 }
 
 #MongoDB环境安装
 If(checkExist mongo.exe 4*){
 	"MongoDB installed"
 } else {
-	downloadAndInstallMSI $MongoDownloadUrl $BasePath mongodb-win32-x86_64-2008plus-ssl-4.0.22-signed.msi ADDLOCAL=ServerService,Server,ProductFeature,Client,MonitoringTools,ImportExportTools,Router,MiscellaneousTools
+	$settings = "ADDLOCAL=ServerService,Server,ProductFeature,Client,MonitoringTools,ImportExportTools,Router,MiscellaneousTools"
+	downloadAndInstallMSI $MongoDownloadUrl $BasePath mongodb-win32-x86_64-2008plus-ssl-4.0.22-signed.msi $settings
 }
 
 #Maven环境安装
