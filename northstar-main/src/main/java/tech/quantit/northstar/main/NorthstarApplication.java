c6367ee942835768fbe99d972d32ee9d1fbd4b67
huangwl <huangwl@kamfu.com.cn>
2022-06-04 16:23:44 +0800

    移除mongo依赖

--- northstar-main/src/main/java/tech/quantit/northstar/main/NorthstarApplication.java
+++ northstar-main/src/main/java/tech/quantit/northstar/main/NorthstarApplication.java
@@ -3,13 +3,11 @@ package tech.quantit.northstar.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @EnableAutoConfiguration
 @SpringBootApplication(scanBasePackages = "tech.quantit.northstar")
-@EnableMongoRepositories("tech.quantit.northstar")
 public class NorthstarApplication {
 	
 	public static void main(String[] args) {
