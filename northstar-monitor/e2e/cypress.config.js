67f7d8c3d0c74fe598681ed1697065870c3b52c9
KevinHuangwl <kevinbauer@163.com>
2022-06-20 23:18:25 +0800

    移动cypress目录

---
+++ northstar-monitor/e2e/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack",
+    },
+  },
+});
