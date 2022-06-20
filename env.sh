b91be4b6536fbf1385c5829f7cb4f3300f7bc0a1
KevinHuangwl <kevinbauer@163.com>
2022-06-20 20:14:50 +0800

    更新环境脚本

--- env.sh
+++ env.sh
@@ -5,14 +5,28 @@ set -e
 echo "准备环境依赖..."
 yum install git nodejs wget python27 gcc gcc-c++ -y
 
+mkdir -p ~/northstar-env
+
+if [[ $(which node >/dev/null && echo $?) != 0 ]];
+then 
+	echo "安装Node16"
+	cd ~/northstar-env && wget --no-check-certificate https://nodejs.org/dist/v16.15.1/node-v16.15.1-linux-x64.tar.xz
+	tar -xvf node-v16.15.1-linux-x64.tar.xz
+	rm -f node-v16.15.1-linux-x64.tar.xz
+	ln -s ~/northstar-env/node-v16.15.1-linux-x64/bin/* /usr/local/bin/
+else
+	echo "Node16已安装"
+	node -v
+fi
+
 # 检查JDK环境
 if [[ $(which java >/dev/null && echo $?) != 0 ]]; 
 then
 	echo "安装JDK17"
-	cd ~ && wget --no-check-certificate https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+	cd ~/northstar-env && wget --no-check-certificate https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
 	tar -xvf jdk-17_linux-x64_bin.tar.gz
 	rm -f jdk-17_linux-x64_bin.tar.gz
-	ln -s ~/$(find jdk* -maxdepth 0 -type d)/bin/* /usr/local/bin/
+	ln -s ~/northstar-env/$(find jdk* -maxdepth 0 -type d)/bin/* /usr/local/bin/
 else
 	echo "JDK17已安装"
 	java -version
@@ -22,11 +36,11 @@ fi
 if [[ $(which mvn >/dev/null && echo $?) != 0 ]]; 
 then
 	echo "安装Maven"
-	cd ~ && wget --no-check-certificate https://mirrors.bfsu.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+	cd ~/northstar-env && wget --no-check-certificate https://mirrors.bfsu.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 	tar -xvf apache-maven-3.6.3-bin.tar.gz
 	rm -f apache-maven-3.6.3-bin.tar.gz
-	ln -s ~/apache-maven-3.6.3/bin/mvn /usr/local/bin/
-	curl https://gitee.com/dromara/northstar/raw/master/settings.xml >~/apache-maven-3.6.3/conf/settings.xml
+	ln -s ~/northstar-env/apache-maven-3.6.3/bin/mvn /usr/local/bin/
+	curl https://gitee.com/dromara/northstar/raw/master/settings.xml >~/northstar-env/apache-maven-3.6.3/conf/settings.xml
 else
 	echo "Maven已安装"
 	mvn -v
@@ -36,13 +50,13 @@ fi
 if [[ $(which redis-server >/dev/null && echo $?) != 0 ]];
 then
 	echo "安装Redis"
-	cd ~ && wget --no-check-certificate http://download.redis.io/releases/redis-7.0.0.tar.gz
+	cd ~/northstar-env && wget --no-check-certificate http://download.redis.io/releases/redis-7.0.0.tar.gz
 	tar -xzf redis-7.0.0.tar.gz
+	rm -f ~/northstar-env/redis-7.0.0.tar.gz
 	cd redis-7.0.0
 	make
 	make install
 	redis-server --daemonize yes
-	rm -f ~/redis-7.0.0.tar.gz
 else
 	echo "Redis已安装"
 fi
