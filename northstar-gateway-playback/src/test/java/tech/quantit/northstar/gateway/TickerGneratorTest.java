d65173d24435aed2668a2a468f6c23b5eeefd0c6
changsong <changsong@wumart.com>
2022-06-22 10:02:58 +0800

    增加测试代码

--- northstar-gateway-playback/src/test/java/tech/quantit/northstar/gateway/TickerGneratorTest.java
+++ northstar-gateway-playback/src/test/java/tech/quantit/northstar/gateway/TickerGneratorTest.java
@@ -1,8 +1,23 @@
 
 package tech.quantit.northstar.gateway;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import tech.quantit.northstar.common.constant.DateTimeConstant;
+import tech.quantit.northstar.gateway.api.domain.BarGenerator;
+import tech.quantit.northstar.gateway.api.domain.NormalContract;
 import tech.quantit.northstar.gateway.playback.ticker.TickerGnerator;
 import test.common.TestFieldFactory;
+import xyz.redtorch.pb.CoreField;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * 测试生产生成Ticker 数据
@@ -13,25 +28,31 @@ public class TickerGneratorTest {
 
     private TickerGnerator tickerGnerator;
 
-    private TestFieldFactory factory = new TestFieldFactory("test");
-    //
-    // @BeforeEach
-    // void prepare() {
-    //     FastEventEngine feEngine = mock(FastEventEngine.class);
-    //     CoreField.GatewaySettingField settings = CoreField.GatewaySettingField.newBuilder()
-    //             .setGatewayId("gatewayId")
-    //             .build();
-    //     SimAccount simAccount = mock(SimAccount.class);
-    //     gateway = new SimTradeGatewayLocal(feEngine, mock(SimMarket.class), settings, "bindedGatewayId", simAccount, mock(GlobalMarketRegistry.class));
-    // }
-    //
-    // @Test
-    // void testConnectAndDisConnect() {
-    //     gateway.connect();
-    //     verify(gateway.feEngine).emitEvent(eq(NorthstarEventType.CONNECTED), anyString());
-    //
-    //     gateway.disconnect();
-    //     verify(gateway.feEngine).emitEvent(eq(NorthstarEventType.DISCONNECTED), anyString());
-    // }
+    TestFieldFactory factory = new TestFieldFactory("testGateway");
+
+    @SuppressWarnings("unchecked")
+    @Test
+    void testRecordsToTicks() {
+        CoreField.ContractField contract = factory.makeContract("rb2210");
+        BarGenerator gen = new BarGenerator(new NormalContract(contract, 0), mock(BiConsumer.class));
+        long now = System.currentTimeMillis();
+        long expectedTime = now - now % 60000 + 60000;
+        LocalDateTime ldt = LocalDateTime.ofInstant(Instant.ofEpochMilli(expectedTime), ZoneId.systemDefault());
+        System.out.println(ldt.toLocalTime().format(DateTimeConstant.T_FORMAT_FORMATTER));
+        CoreField.TickField tick1 = factory.makeTickField("rb2210", 1000);
+
+        gen.update(tick1);
+
+        CoreField.BarField bar = gen.finishOfBar();
+        assertThat(bar.getActionTimestamp()).isEqualTo(expectedTime);
+        assertThat(bar.getActionTime()).isEqualTo(ldt.toLocalTime().format(DateTimeConstant.T_FORMAT_WITH_MS_INT_FORMATTER));
+
+        List<CoreField.BarField> records = Lists.newArrayList();
+        records.add(bar);
+        List<CoreField.TickField> tickFieldList = TickerGnerator.recordsToTicks(1000, records);
+
+        assertThat(tickFieldList.size()).isEqualTo(3);
+    }
+
 
 }
