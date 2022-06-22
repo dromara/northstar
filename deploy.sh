ac022e76c81d5b9787f57716a6e88daa0452f916
KevinHuangwl <kevinbauer@163.com>
2022-06-22 23:38:17 +0800

    更新脚本

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,6 @@ if [[ `pgrep -a java | grep northstar.jar | wc -l` > 0 ]]; then
 fi
 
 mvn clean install -Dmaven.test.skip=true
+cp startup.sh ~/northstar-dist/
 \mv -f northstar-main/target/northstar-*.jar ~/northstar-dist/northstar.jar
+
