ab760d6ab4a5e8197b9020226481916d2830d141
KevinHuangwl <kevinbauer@163.com>
2022-08-22 20:08:43 +0800

    修正平仓优化实现

--- northstar-domain/src/main/java/tech/quantit/northstar/domain/module/PriorBeforeAndHedgeTodayClosingStrategy.java
+++ northstar-domain/src/main/java/tech/quantit/northstar/domain/module/PriorBeforeAndHedgeTodayClosingStrategy.java
@@ -3,7 +3,9 @@ package tech.quantit.northstar.domain.module;
 import tech.quantit.northstar.common.constant.ClosingPolicy;
 import tech.quantit.northstar.common.constant.SignalOperation;
 import tech.quantit.northstar.strategy.api.ClosingStrategy;
+import xyz.redtorch.pb.CoreEnum.ExchangeEnum;
 import xyz.redtorch.pb.CoreEnum.OffsetFlagEnum;
+import xyz.redtorch.pb.CoreField.ContractField;
 import xyz.redtorch.pb.CoreField.PositionField;
 
 public class PriorBeforeAndHedgeTodayClosingStrategy  implements ClosingStrategy{
@@ -11,7 +13,16 @@ public class PriorBeforeAndHedgeTodayClosingStrategy  implements ClosingStrategy
 	@Override
 	public OffsetFlagEnum resolveOperation(SignalOperation opr, PositionField position) {
 		if(opr.isOpen())	return OffsetFlagEnum.OF_Open;
-		if(position.getYdPosition() - position.getYdFrozen() > 0)	return OffsetFlagEnum.OF_CloseYesterday;
+		if(position.getPosition() - position.getFrozen() < 1) {
+			throw new IllegalStateException("没有足够持仓可对冲");
+		}
+		ContractField contract = position.getContract();
+		if(position.getYdPosition() - position.getYdFrozen() > 0) {			
+			if(contract.getExchange() == ExchangeEnum.SHFE) {			
+				return OffsetFlagEnum.OF_CloseYesterday;
+			}
+			return OffsetFlagEnum.OF_Close;
+		}
 		return OffsetFlagEnum.OF_Open;
 	}
 	
--- northstar-domain/src/main/java/tech/quantit/northstar/domain/module/PriorTodayClosingStrategy.java
+++ northstar-domain/src/main/java/tech/quantit/northstar/domain/module/PriorTodayClosingStrategy.java
@@ -3,7 +3,9 @@ package tech.quantit.northstar.domain.module;
 import tech.quantit.northstar.common.constant.ClosingPolicy;
 import tech.quantit.northstar.common.constant.SignalOperation;
 import tech.quantit.northstar.strategy.api.ClosingStrategy;
+import xyz.redtorch.pb.CoreEnum.ExchangeEnum;
 import xyz.redtorch.pb.CoreEnum.OffsetFlagEnum;
+import xyz.redtorch.pb.CoreField.ContractField;
 import xyz.redtorch.pb.CoreField.PositionField;
 
 public class PriorTodayClosingStrategy  implements ClosingStrategy{
@@ -11,8 +13,15 @@ public class PriorTodayClosingStrategy  implements ClosingStrategy{
 	@Override
 	public OffsetFlagEnum resolveOperation(SignalOperation opr, PositionField position) {
 		if(opr.isOpen())	return OffsetFlagEnum.OF_Open;
-		if(position.getTdPosition() - position.getTdFrozen() > 0)	return OffsetFlagEnum.OF_CloseToday;
-		return OffsetFlagEnum.OF_CloseYesterday;
+		if(position.getPosition() - position.getFrozen() < 1) {
+			throw new IllegalStateException("没有足够持仓可用");
+		}
+		ContractField contract = position.getContract();
+		if(contract.getExchange() == ExchangeEnum.SHFE) {			
+			if(position.getTdPosition() - position.getTdFrozen() > 0)	return OffsetFlagEnum.OF_CloseToday;
+			if(position.getYdPosition() - position.getYdFrozen() > 0)	return OffsetFlagEnum.OF_CloseYesterday;
+		}
+		return OffsetFlagEnum.OF_Close;
 	}
 
 	@Override
