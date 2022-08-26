f8bc27c7ae339750bf6cf9be7286be237a4a0dc8
KevinHuangwl <kevinbauer@163.com>
2022-08-26 08:54:07 +0800

    更新

--- northstar-gateway-ctp/src/main/java/tech/quantit/northstar/CtpGatewaySettings.java
+++ northstar-gateway-ctp/src/main/java/tech/quantit/northstar/CtpGatewaySettings.java
@@ -29,7 +29,7 @@ public class CtpGatewaySettings extends DynamicParams implements GatewaySettings
 	@Setting(label = "网关密码", type = FieldType.PASSWORD, order = 20)
 	private String password;
 	
-	@Setting(label = "期货公司", type = FieldType.SELECT, options = {"宏源主席", "宏源次席", "平安主席"}, optionsVal = {"1080", "2070", "5200"}, placeholder = "请选择", order = 30)
+	@Setting(label = "期货公司", type = FieldType.SELECT, options = {"平安主席", "宏源主席", "宏源次席"}, optionsVal = {"5200", "1080", "2070"}, placeholder = "请选择", order = 30)
 	private String brokerId;
 
 	@Override
