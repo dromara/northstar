0885769b70847c36f086361b3f93cfee55a177fc
KevinHuangwl <kevinbauer@163.com>
2022-09-13 22:45:48 +0800

    增加指标值可变方案

--- northstar-common/src/main/java/tech/quantit/northstar/common/model/TimeSeriesValue.java
+++ northstar-common/src/main/java/tech/quantit/northstar/common/model/TimeSeriesValue.java
@@ -1,16 +1,36 @@
 package tech.quantit.northstar.common.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class TimeSeriesValue implements Comparable<TimeSeriesValue> {
-
+	
 	private double value;
 	
 	private long timestamp;
+	
+	private boolean isSettled = true;
+	
+	/**
+	 * @param value			时序值
+	 * @param timestamp		值对应的时间
+	 */
+	public TimeSeriesValue(double value, long timestamp) {
+		this.value = value;
+		this.timestamp = timestamp;
+	}
 
+	/**
+	 * @param value			时序值
+	 * @param timestamp		值对应的时间
+	 * @param isSettled		是否为确定值（不会再发生变化）
+	 */
+	public TimeSeriesValue(double value, long timestamp, boolean isSettled) {
+		this.value = value;
+		this.timestamp = timestamp;
+		this.isSettled = isSettled;
+	}
+	
 	@Override
 	public int compareTo(TimeSeriesValue o) {
 		return value < o.value ? -1 : 1;
