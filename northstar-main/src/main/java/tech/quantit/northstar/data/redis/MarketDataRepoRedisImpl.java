467ad75c92a1fcb7aa5c546f87d286ff9aaef9e5
KevinHuangwl <kevinbauer@163.com>
2022-09-30 23:08:34 +0800

    修复当天为周五时加载数据时出现异常的问题

--- northstar-main/src/main/java/tech/quantit/northstar/data/redis/MarketDataRepoRedisImpl.java
+++ northstar-main/src/main/java/tech/quantit/northstar/data/redis/MarketDataRepoRedisImpl.java
@@ -67,14 +67,15 @@ public class MarketDataRepoRedisImpl extends MarketDataRepoDataServiceImpl {
 	public List<BarField> loadBars(String gatewayId, String unifiedSymbol, LocalDate startDate, LocalDate endDate0) {
 		log.debug("加载 [{}] 历史行情数据：{}，{} -> {}", gatewayId, unifiedSymbol, startDate.format(DateTimeConstant.D_FORMAT_INT_FORMATTER), endDate0.format(DateTimeConstant.D_FORMAT_INT_FORMATTER));
 		LocalDate today = LocalDate.now();
-		LocalDate endDate = today.isAfter(endDate0) ? endDate0 : today; 
+		LocalDate endDate = today.isAfter(endDate0) ? endDate0 : today;
 		LinkedList<BarField> resultList = new LinkedList<>();
-		List<BarField> list = super.loadBars(gatewayId, unifiedSymbol, startDate, endDate)
-				.stream()
-				.sorted((a, b) -> a.getActionTimestamp() < b.getActionTimestamp() ? -1 : 1)
-				.toList();
-		resultList.addAll(list);
-		
+		if(endDate.isAfter(startDate)) {
+			List<BarField> list = super.loadBars(gatewayId, unifiedSymbol, startDate, endDate)
+					.stream()
+					.sorted((a, b) -> a.getActionTimestamp() < b.getActionTimestamp() ? -1 : 1)
+					.toList();
+			resultList.addAll(list);
+		}
 		if(today.isAfter(endDate0))	return resultList; 
 		
 		LocalDate localQueryDate = today;
