8345966b93393724946ba17fcd2ee4234dc86f83
KevinHuangwl <kevinbauer@163.com>
2022-11-04 20:35:38 +0800

    修复策略层异常没有把异常捕获并输出到相应模组的问题

--- northstar-main/src/main/java/tech/quantit/northstar/domain/module/ModuleContext.java
+++ northstar-main/src/main/java/tech/quantit/northstar/domain/module/ModuleContext.java
@@ -147,9 +147,13 @@ public class ModuleContext implements IModuleContext{
 				indicatorValBufQMap.get(e.getKey()).offer(indicator.timeSeriesValue(0));	
 			}
 		};
-		indicatorFactory.getIndicatorMap().entrySet().stream().forEach(action);	// 记录常规指标更新值 
-		tradeStrategy.onBar(bar, module.isEnabled());
-		inspectedValIndicatorFactory.getIndicatorMap().entrySet().stream().forEach(action);	// 记录透视值更新
+		try {			
+			indicatorFactory.getIndicatorMap().entrySet().stream().forEach(action);	// 记录常规指标更新值 
+			tradeStrategy.onBar(bar, module.isEnabled());
+			inspectedValIndicatorFactory.getIndicatorMap().entrySet().stream().forEach(action);	// 记录透视值更新
+		} catch(Exception e) {
+			getLogger().error("", e);
+		}
 		if(barBufQMap.get(bar.getUnifiedSymbol()).size() >= bufSize.intValue()) {
 			barBufQMap.get(bar.getUnifiedSymbol()).poll();
 		}
