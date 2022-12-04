279c70fe530f738dac350a9323d9e836eae37f40
KevinHuangwl <kevinbauer@163.com>
2022-12-04 16:50:22 +0800

    更新启动脚本

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 启动服务
-nohup java -Xlog:gc*:gc.log -Xmn1g -Xmx1g -jar ~/northstar-dist/northstar*.jar >ns.log &
+nohup java -Xlog:gc*:gc.log -Xmn2g -Xmx2g -jar ~/northstar-dist/northstar*.jar >ns.log &
