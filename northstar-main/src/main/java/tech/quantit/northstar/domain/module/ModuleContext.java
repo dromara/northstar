e3f6b211e4e5b76c2f61caf19bab4aa61653379a
KevinHuangwl <kevinbauer@163.com>
2023-01-06 20:46:36 +0800

    修复跨周期部分指标值打图不正确的问题

--- northstar-main/src/main/java/tech/quantit/northstar/domain/module/ModuleContext.java
+++ northstar-main/src/main/java/tech/quantit/northstar/domain/module/ModuleContext.java
@@ -70,6 +70,7 @@ import tech.quantit.northstar.strategy.api.indicator.Indicator.PeriodUnit;
 import tech.quantit.northstar.strategy.api.indicator.Indicator.ValueType;
 import tech.quantit.northstar.strategy.api.indicator.TimeSeriesUnaryOperator;
 import tech.quantit.northstar.strategy.api.log.ModuleLoggerFactory;
+import tech.quantit.northstar.strategy.api.utils.bar.BarMerger;
 import tech.quantit.northstar.strategy.api.utils.bar.BarMergerRegistry;
 import tech.quantit.northstar.strategy.api.utils.bar.BarMergerRegistry.CallbackPriority;
 import tech.quantit.northstar.strategy.api.utils.trade.DealCollector;
@@ -155,6 +156,7 @@ public class ModuleContext implements IModuleContext, MergedBarListener{
 	private final String moduleName;
 	
 	private final BarMergerRegistry registry = new BarMergerRegistry();
+	private final Set<BarMerger> ctxBarMerger = new HashSet<>();
 	
 	private Logger mlog;
 	
@@ -469,6 +471,7 @@ public class ModuleContext implements IModuleContext, MergedBarListener{
 		comboIndicators.stream().forEach(combo -> combo.onBar(bar));
 		inspectedValIndicatorFactory.getIndicatorMap().entrySet().stream().forEach(e -> e.getValue().onBar(bar));	// 值透视指标的更新
 		registry.onBar(bar);
+		ctxBarMerger.forEach(merger -> merger.onBar(bar));
 	}
 	
 	@Override
@@ -574,7 +577,8 @@ public class ModuleContext implements IModuleContext, MergedBarListener{
 			contractMap2.put(c, contract);
 			barBufQMap.put(c.getUnifiedSymbol(), new LinkedList<>());
 			bindedSymbolSet.add(c.getUnifiedSymbol());
-			registry.addListener(contract, numOfMinsPerBar, PeriodUnit.MINUTE, this, CallbackPriority.FOUR);
+			
+			ctxBarMerger.add(new BarMerger(numOfMinsPerBar, contract, (merger, bar) -> this.onMergedBar(bar)));
 		}
 	}
 	
