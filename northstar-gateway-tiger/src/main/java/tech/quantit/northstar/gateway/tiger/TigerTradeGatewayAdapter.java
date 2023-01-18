42bd9c219c5ac5179e8342c0103b53f10d8c1fea
KevinHuangwl <kevinbauer@163.com>
2023-01-18 16:56:19 +0800

    更新

--- northstar-gateway-tiger/src/main/java/tech/quantit/northstar/gateway/tiger/TigerTradeGatewayAdapter.java
+++ northstar-gateway-tiger/src/main/java/tech/quantit/northstar/gateway/tiger/TigerTradeGatewayAdapter.java
@@ -4,10 +4,12 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -28,6 +30,7 @@ import com.tigerbrokers.stock.openapi.client.struct.enums.Language;
 import com.tigerbrokers.stock.openapi.client.struct.enums.MethodName;
 import com.tigerbrokers.stock.openapi.client.struct.enums.OrderType;
 import com.tigerbrokers.stock.openapi.client.struct.enums.SecType;
+import com.tigerbrokers.stock.openapi.client.struct.param.OrderParameter;
 import com.tigerbrokers.stock.openapi.client.util.builder.AccountParamBuilder;
 import com.tigerbrokers.stock.openapi.client.util.builder.TradeParamBuilder;
 
@@ -46,6 +49,7 @@ import xyz.redtorch.pb.CoreEnum.PositionDirectionEnum;
 import xyz.redtorch.pb.CoreEnum.TimeConditionEnum;
 import xyz.redtorch.pb.CoreField.AccountField;
 import xyz.redtorch.pb.CoreField.CancelOrderReqField;
+import xyz.redtorch.pb.CoreField.ContractField;
 import xyz.redtorch.pb.CoreField.OrderField;
 import xyz.redtorch.pb.CoreField.PositionField;
 import xyz.redtorch.pb.CoreField.SubmitOrderReqField;
@@ -64,7 +68,7 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 	private final TigerGatewaySettings settings;
 	private final IContractManager contractMgr;
 	
-	private final ConcurrentMap<Long, Integer> orderIdVolMap = new ConcurrentHashMap<>();
+	private final Set<Long> pendingOrder = new HashSet<>();
 	
 	private TigerHttpClient client;
 	private Timer timer;
@@ -73,9 +77,13 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 		
 		@Override
 		public void run() {
-			queryOrder(false);
-			queryAccount();
-			queryPosition();
+			try {
+				queryOrder(false);
+				queryAccount();
+				queryPosition();
+			} catch(Exception e) {
+				log.error("", e);
+			}
 		}
 	};
 	
@@ -99,7 +107,10 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 		feEngine.emitEvent(NorthstarEventType.CONNECTED, gatewayId());
 		feEngine.emitEvent(NorthstarEventType.LOGGED_IN, gatewayId());
 		timer = new Timer("TIGER_" + gatewayId(), true);
-		timer.scheduleAtFixedRate(task, 0, 1000);
+		CompletableFuture.runAsync(() -> {
+			queryOrder(true);
+			timer.scheduleAtFixedRate(task, 0, 1000);
+		}, CompletableFuture.delayedExecutor(3, TimeUnit.SECONDS));
 	}
 
 	@Override
@@ -114,6 +125,10 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 		log.trace("查询TIGER账户信息");
 		PrimeAssetRequest assetRequest = PrimeAssetRequest.buildPrimeAssetRequest(settings.getAccountId());
 		PrimeAssetResponse primeAssetResponse = client.execute(assetRequest);
+		if(!primeAssetResponse.isSuccess()) {
+			log.warn("查询账户返回异常：{}", primeAssetResponse.getMessage());
+			return;
+		}
 		//查询证券相关资产信息
 		PrimeAssetItem.Segment segment = primeAssetResponse.getSegment(Category.S);	// 暂不实现期货资产查询 
 		//查询账号中美元相关资产信息
@@ -142,15 +157,21 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 
 		request.setBizContent(bizContent);
 		TigerHttpResponse response = client.execute(request);
-
+		if(!response.isSuccess()) {
+			log.warn("查询持仓返回异常：{}", response.getMessage());
+			return;
+		}
 		// 解析具体字段
 		JSONArray positions = JSON.parseObject(response.getData()).getJSONArray("items");
 		for(int i=0; i<positions.size(); i++) {
 			JSONObject json = positions.getJSONObject(i);
+			ContractField contract = contractMgr.getContract("TIGER", json.getString("symbol")).contractField();
+			String positionId = String.format("%s@%s@%s", contract.getUnifiedSymbol(), PositionDirectionEnum.PD_Long, gatewayId());
 			feEngine.emitEvent(NorthstarEventType.POSITION, PositionField.newBuilder()
 					.setAccountId(settings.getAccountId())
 					.setGatewayId(gatewayId())
-					.setContract(contractMgr.getContract("TIGER", json.getString("symbol")).contractField())
+					.setPositionId(positionId)
+					.setContract(contract)
 					.setOpenPositionProfit(json.getDoubleValue("unrealizedPnl"))
 					.setPosition(json.getIntValue("position"))
 					.setOpenPrice(json.getDoubleValue("averageCost"))
@@ -170,12 +191,16 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 
 		request.setBizContent(bizContent);
 		TigerHttpResponse response = client.execute(request);
+		if(!response.isSuccess()) {
+			log.warn("查询订单返回异常：{}", response.getMessage());
+			return;
+		}
 		JSONObject data = JSON.parseObject(response.getData());
 		JSONArray items = data.getJSONArray("items");
 		for(int i=0; i<items.size(); i++) {
 			JSONObject json = items.getJSONObject(i);
 			Long id = json.getLong("id");
-			if(!orderIdVolMap.containsKey(id) && !showAll) {
+			if(!pendingOrder.contains(id) && !showAll) {
 				continue;
 			}
 			
@@ -195,8 +220,7 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 			OrderStatusEnum orderStatus = switch(json.getString("status")) {
 			case "Filled" -> OrderStatusEnum.OS_AllTraded;
 			case "Cancelled" -> OrderStatusEnum.OS_Canceled;
-			case "Initial" -> OrderStatusEnum.OS_Touched;
-			case "PendingSubmit", "Submitted" -> OrderStatusEnum.OS_NoTradeNotQueueing;
+			case "Initial", "PendingSubmit", "Submitted" -> OrderStatusEnum.OS_Touched;
 			case "Invalid", "Inactive" -> OrderStatusEnum.OS_Rejected;
 			default -> throw new IllegalArgumentException("Unexpected value: " + json.getString("status"));
 			};
@@ -214,10 +238,15 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 			String symbol = json.getString("symbol");
 			int tradedVol = json.getIntValue("filledQuantity");
 			int totalVol = json.getIntValue("totalQuantity");
+			ContractField contract = contractMgr.getContract("TIGER", symbol).contractField();
+			double price = (int) (json.getDoubleValue("limitPrice") / contract.getPriceTick()) * contract.getPriceTick();
+			if(orderStatus == OrderStatusEnum.OS_Touched) {
+				pendingOrder.add(id);
+			}
 			OrderField order = OrderField.newBuilder()
 					.setAccountId(settings.getAccountId())
 					.setGatewayId(gatewayId())
-					.setContract(contractMgr.getContract("TIGER", symbol).contractField())
+					.setContract(contract)
 					.setOriginOrderId(orderId)
 					.setOrderId(orderId)
 					.setDirection(direction)
@@ -225,7 +254,7 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 					.setOrderDate(bizContent)
 					.setTotalVolume(totalVol)
 					.setTradedVolume(tradedVol)
-					.setPrice(json.getDoubleValue("limitPrice"))
+					.setPrice(price)
 					.setTradingDay(tradingDay)
 					.setOrderDate(orderDate)
 					.setOrderTime(LocalDateTime.ofInstant(ins, ZoneId.systemDefault()).toLocalTime().format(DateTimeConstant.T_FORMAT_FORMATTER))
@@ -235,35 +264,45 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 			feEngine.emitEvent(NorthstarEventType.ORDER, order);
 			
 			if(tradedVol > 0) {
-				queryTrade(order);
+				queryTrade(order, showAll);
 			}
-			if(tradedVol == totalVol && orderIdVolMap.containsKey(id)) {
-				orderIdVolMap.remove(id);
+			if(tradedVol == totalVol && pendingOrder.contains(id)) {
+				pendingOrder.remove(id);
 			}
 		}
 	}
 	
-	private void queryTrade(OrderField order) {
+	private void queryTrade(OrderField order, boolean showAll) {
 		log.trace("查询TIGER成交信息");	
 		
 		TigerHttpRequest request = new TigerHttpRequest(MethodName.ORDER_TRANSACTIONS);
+		// FIXME 还是要根据OrderId去查询比较靠谱，但由于接口异常原因暂时无法按orderId查询。按名称查询如果结果集过大处理会比较麻烦
 		String bizContent = AccountParamBuilder.instance()
 		    .account(settings.getAccountId())
 		    .secType(SecType.STK)
-		    .orderId(Long.valueOf(order.getOrderId()))
-		    .limit(30)
+		    .symbol(order.getContract().getSymbol())
+		    .limit(100)
 		    .buildJson();
 		request.setBizContent(bizContent);
 		TigerHttpResponse response = client.execute(request);
+		if(!response.isSuccess()) {
+			log.warn("查询成交返回异常：{}", response.getMessage());
+			return;
+		}
 
 		JSONArray data = JSON.parseObject(response.getData()).getJSONArray("items");
 		for(int i=0; i<data.size(); i++) {
 			JSONObject json = data.getJSONObject(i);
 			String symbol = json.getString("symbol");
+			long orderId = json.getLongValue("orderId");
+			if(!showAll && orderId != Long.parseLong(order.getOrderId())) {
+				continue;
+			}
 			
 			feEngine.emitEvent(NorthstarEventType.TRADE, TradeField.newBuilder()
 					.setAccountId(bizContent)
 					.setGatewayId(gatewayId())
+					.setTradeId(order.getOrderId())
 					.setOrderId(order.getOrderId())
 					.setOriginOrderId(order.getOriginOrderId())
 					.setDirection(order.getDirection())
@@ -325,8 +364,8 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 		log.info("网关[{}] 下单：{}", gatewayId(), model);
 		TradeOrderResponse response = client.execute(TradeOrderRequest.newRequest(model));
 		log.info("网关[{}] 下单反馈：{}", gatewayId(), JSON.toJSONString(response));
-		Long id = response.getItem().getOrderId();
-		orderIdVolMap.putIfAbsent(id, submitOrderReq.getVolume());
+		Long id = response.getItem().getId();
+		pendingOrder.add(id);
 		return id + "";
 	}
 
@@ -335,13 +374,14 @@ public class TigerTradeGatewayAdapter implements TradeGateway{
 		if(!isConnected()) {
 			throw new IllegalStateException("网关未连线");
 		}
-		String bizContent = TradeParamBuilder.instance().account(settings.getAccountId()).id(Long.valueOf(cancelOrderReq.getOriginOrderId())).secretKey(settings.getSecretKey()).buildJson();
+		OrderParameter params = TradeParamBuilder.instance().account(settings.getAccountId()).id(Long.valueOf(cancelOrderReq.getOriginOrderId())).secretKey(settings.getSecretKey()).build();
+		String bizContent = JSON.toJSONString(params);
 		TigerHttpRequest request = new TigerHttpRequest(MethodName.CANCEL_ORDER);
 		request.setBizContent(bizContent);
 		log.info("网关[{}] 撤单：{}", gatewayId(), bizContent);
 		TigerHttpResponse response = client.execute(request);
 		if(response.isSuccess()) {
-			orderIdVolMap.remove(Long.valueOf(cancelOrderReq.getOriginOrderId()));
+			pendingOrder.remove(Long.valueOf(cancelOrderReq.getOriginOrderId()));
 		}
 		log.info("网关[{}] 撤单反馈：{}", gatewayId(), JSON.toJSONString(response));
 		return response.isSuccess();
