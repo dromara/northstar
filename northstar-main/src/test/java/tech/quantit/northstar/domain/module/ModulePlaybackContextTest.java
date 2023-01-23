fca37a8efd84f831689e74bb85458529008167f4
KevinHuangwl <kevinbauer@163.com>
2023-01-23 22:03:42 +0800

    修复单元测试

--- northstar-main/src/test/java/tech/quantit/northstar/domain/module/ModulePlaybackContextTest.java
+++ northstar-main/src/test/java/tech/quantit/northstar/domain/module/ModulePlaybackContextTest.java
@@ -2,14 +2,12 @@ package tech.quantit.northstar.domain.module;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +18,6 @@ import com.alibaba.fastjson.JSONObject;
 import tech.quantit.northstar.common.constant.Constants;
 import tech.quantit.northstar.common.constant.ModuleState;
 import tech.quantit.northstar.common.constant.SignalOperation;
-import tech.quantit.northstar.common.exception.TradeException;
 import tech.quantit.northstar.common.model.ModuleAccountRuntimeDescription;
 import tech.quantit.northstar.common.model.ModuleRuntimeDescription;
 import tech.quantit.northstar.gateway.api.IContractManager;
@@ -32,9 +29,9 @@ import tech.quantit.northstar.strategy.api.IModule;
 import tech.quantit.northstar.strategy.api.IModuleAccountStore;
 import tech.quantit.northstar.strategy.api.TradeStrategy;
 import tech.quantit.northstar.strategy.api.constant.PriceType;
-import tech.quantit.northstar.strategy.api.indicator.TimeSeriesUnaryOperator;
 import tech.quantit.northstar.strategy.api.indicator.Indicator.Configuration;
 import tech.quantit.northstar.strategy.api.indicator.Indicator.ValueType;
+import tech.quantit.northstar.strategy.api.indicator.TimeSeriesUnaryOperator;
 import tech.quantit.northstar.strategy.api.indicator.function.AverageFunctions;
 import tech.quantit.northstar.strategy.api.utils.trade.DealCollector;
 import tech.quantit.northstar.strategy.api.utils.trade.TradeIntent;
@@ -45,15 +42,15 @@ import xyz.redtorch.pb.CoreField.ContractField;
 import xyz.redtorch.pb.CoreField.TickField;
 import xyz.redtorch.pb.CoreField.TradeField;
 
-class ModuleContextTest {
-	
-	private TestFieldFactory factory = new TestFieldFactory("testGateway");
+class ModulePlaybackContextTest {
+
+private TestFieldFactory factory = new TestFieldFactory("回测账户");
 	
 	private ContractField contract = factory.makeContract("rb2205");
 	
 	private TickField tick = factory.makeTickField("rb2205", 5010);
 	
-	private ModuleContext ctx;
+	private ModulePlaybackContext ctx;
 	
 	private final static String NAME = "testModule";
 	
@@ -72,9 +69,9 @@ class ModuleContextTest {
 		
 		ClosingStrategy closingStrategy = new FirstInFirstOutClosingStrategy();
 		Map<String, ModuleAccountRuntimeDescription> accRtsMap = new HashMap<>();
-		accRtsMap.put("testAccout", ModuleAccountRuntimeDescription.builder()
+		accRtsMap.put("回测账户", ModuleAccountRuntimeDescription.builder()
 				.initBalance(10000)
-				.accountId("testGateway")
+				.accountId("回测账户")
 				.build());
 		ModuleRuntimeDescription mrd = ModuleRuntimeDescription.builder()
 				.moduleName(NAME)
@@ -85,8 +82,7 @@ class ModuleContextTest {
 				.build();
 		IModuleAccountStore accStore = new ModuleAccountStore(NAME, closingStrategy.getClosingPolicy(), mrd, contractMgr);
 		
-		ctx = new ModuleContext(NAME, strategy, accStore, closingStrategy, 3, 100, mock(DealCollector.class), 
-				mock(Consumer.class), mock(Consumer.class), mock(BiConsumer.class));
+		ctx = new ModulePlaybackContext(NAME, strategy, accStore, 3, 100, mock(DealCollector.class), mock(Consumer.class), mock(Consumer.class));
 		ctx.setModule(module);
 		ctx.bindGatewayContracts(gateway, List.of(c));
 		ctx.onTrade(TradeField.newBuilder()
@@ -101,25 +97,6 @@ class ModuleContextTest {
 				.build());
 	}
 
-	@Test
-	void testSubmitOrderReqWithException() {
-		ctx.onTick(tick);
-		assertThrows(TradeException.class, () -> {
-			ctx.submitOrderReq(contract, SignalOperation.BUY_OPEN, PriceType.LIMIT_PRICE, 10, 5000);
-		});
-		
-		TradeIntent ti = TradeIntent.builder()
-				.contract(contract)
-				.operation(SignalOperation.BUY_OPEN)
-				.volume(10)
-				.priceType(PriceType.ANY_PRICE)
-				.build();
-		ctx.submitOrderReq(ti);
-		assertThrows(TradeException.class, () -> {
-			ctx.onTick(tick);
-		});
-	}
-	
 	@Test
 	void testSubmitOrderReq() {
 		ctx.onTick(tick);
@@ -146,7 +123,7 @@ class ModuleContextTest {
 	@Test
 	void testHoldingNetProfit() {
 		ctx.onTick(tick);
-		assertThat(ctx.holdingNetProfit()).isEqualTo(100);
+		assertThat(ctx.holdingNetProfit()).isEqualTo(0);
 	}
 	
 	@Test
@@ -158,7 +135,7 @@ class ModuleContextTest {
 	
 	@Test
 	void testAvailablePosition() {
-		assertThat(ctx.availablePosition(DirectionEnum.D_Buy, contract.getUnifiedSymbol())).isEqualTo(1);
+		assertThat(ctx.availablePosition(DirectionEnum.D_Buy, contract.getUnifiedSymbol())).isZero();
 		assertThat(ctx.availablePosition(DirectionEnum.D_Sell, contract.getUnifiedSymbol())).isZero();
 	}
 	
@@ -176,4 +153,5 @@ class ModuleContextTest {
 			ctx.newIndicator(Configuration.builder().bindedContract(contract).indicatorName("VOL").build(), ValueType.VOL, TimeSeriesUnaryOperator.identity());
 		});
 	}
+
 }
