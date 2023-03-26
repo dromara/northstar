5ef8f5fd329a0b8d471b6bc98d36a294c4f2d114
KevinHuangwl <kevinbauer@163.com>
2023-03-26 14:38:32 +0800

    恢复两个误删除的类

---
+++ northstar-gateway-api/src/test/java/tech/quantit/northstar/gateway/api/domain/time/BarClockTest.java
@@ -0,0 +1,35 @@
+package tech.quantit.northstar.gateway.api.domain.time;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class BarClockTest {
+
+	BarClock clock = new BarClock(List.of(
+			LocalTime.of(22, 0), LocalTime.of(23, 0), LocalTime.of(10, 0), 
+			LocalTime.of(11, 15), LocalTime.of(14, 15), LocalTime.of(15, 0)));
+	
+	@Test
+	void testAdjustTime() {
+		assertThat(clock.adjustTime(LocalTime.of(21, 0))).isFalse();
+		assertThat(clock.adjustTime(LocalTime.of(22, 0))).isFalse();
+		assertThat(clock.adjustTime(LocalTime.of(10, 30))).isTrue();
+	}
+	
+	@Test
+	void test() {
+		assertThat(clock.adjustTime(LocalTime.of(21, 0))).isFalse();
+		assertThat(clock.currentTimeBucket()).isEqualTo(LocalTime.of(22, 0));
+		assertThat(clock.next()).isEqualTo(LocalTime.of(23, 0));
+		assertThat(clock.currentTimeBucket()).isEqualTo(LocalTime.of(23, 0));
+		
+		assertThat(clock.adjustTime(LocalTime.of(10, 30))).isTrue();
+		assertThat(clock.currentTimeBucket()).isEqualTo(LocalTime.of(11, 15));
+		assertThat(clock.next()).isEqualTo(LocalTime.of(14, 15));
+	}
+
+}
