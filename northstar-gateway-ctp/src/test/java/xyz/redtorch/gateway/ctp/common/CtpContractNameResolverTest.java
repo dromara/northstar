b4fd08dac39ada617bc4e577614aa721ee65009e
KevinHuangwl <kevinbauer@163.com>
2023-04-07 15:22:47 +0800

    修复期权合约名称中文名称不统一问题

--- northstar-gateway-ctp/src/test/java/xyz/redtorch/gateway/ctp/common/CtpContractNameResolverTest.java
+++ northstar-gateway-ctp/src/test/java/xyz/redtorch/gateway/ctp/common/CtpContractNameResolverTest.java
@@ -11,6 +11,11 @@ class CtpContractNameResolverTest {
 		assertThat(CtpContractNameResolver.getCNSymbolName("rb2210")).isEqualTo("螺纹钢2210");
 		assertThat(CtpContractNameResolver.getCNSymbolName("rb0000")).isEqualTo("螺纹钢0000");
 		assertThat(CtpContractNameResolver.getCNSymbolName("rb")).isEqualTo("螺纹钢");
+		
+		assertThat(CtpContractNameResolver.getCNSymbolName("i2306-C-680")).isEqualTo("铁矿石2306买权680");
+		assertThat(CtpContractNameResolver.getCNSymbolName("TA306C4700")).isEqualTo("PTA306买权4700");
+		assertThat(CtpContractNameResolver.getCNSymbolName("si2310-P-13000")).isEqualTo("工业硅2310卖权13000");
+		assertThat(CtpContractNameResolver.getCNSymbolName("MA309P2225")).isEqualTo("甲醇309卖权2225");
 	}
 
 	@Test
