44d6c9b375a9ae80d61b67f55efad5bfe2cb7019
KevinHuangwl <kevinbauer@163.com>
2023-04-26 23:41:22 +0800

    更正指标计算

--- northstar-api/src/main/java/org/dromara/northstar/indicator/EMAIndicator.java
+++ northstar-api/src/main/java/org/dromara/northstar/indicator/EMAIndicator.java
@@ -3,29 +3,30 @@ package org.dromara.northstar.indicator;
 import java.util.Collections;
 import java.util.List;
 
-public class EMAIndicator extends AbstractMAIndicator implements Indicator{
-	
-	private Num lastVal;
+public class EMAIndicator extends AbstractIndicator implements Indicator{
 	
 	private double factor;
 	
 	public EMAIndicator(Configuration cfg, int barCount) {
-		super(cfg, barCount);
-		factor = 2D / (barCount + 1);	// EMA 的更新系数
+		super(cfg);
+		this.factor = 2D / (barCount + 1);	// EMA 的更新系数
 	}
 
 	@Override
 	public List<Indicator> dependencies() {
 		return Collections.emptyList();
 	}
-
+	
 	@Override
-	protected Num evaluate(Num num) {
-		if(sample.size() == 0 || sample.size() == 1 && sample.get().unstable()) {	// 当计算样本没有值，或只有一个不稳定值时
-			lastVal = num;
+	public void update(Num num) {
+		super.update(evaluate(num));
+	}
+
+	private Num evaluate(Num num) {
+		if(ringBuf.size() == 0 || ringBuf.size() == 1 && ringBuf.get().unstable()) {	// 当计算样本没有值，或只有一个不稳定值时
 			return num;
 		}
-		double preVal = lastVal.unstable() ? sample.get(-1).value() : sample.get(0).value(); 
+		double preVal = ringBuf.get().unstable() ? ringBuf.get(-1).value() : ringBuf.get(0).value(); 
 		double val = factor * num.value() + (1 - factor) * preVal;
 		return Num.of(val, num.unstable());
 	}
--- northstar-api/src/main/java/org/dromara/northstar/indicator/MAIndicator.java
+++ northstar-api/src/main/java/org/dromara/northstar/indicator/MAIndicator.java
@@ -3,12 +3,15 @@ package org.dromara.northstar.indicator;
 import java.util.Collections;
 import java.util.List;
 
-public class MAIndicator extends AbstractMAIndicator implements Indicator {
+public class MAIndicator extends AbstractIndicator implements Indicator {
 
+	private RingArray<Num> sample;
+	
 	private double sum;
 	
 	public MAIndicator(Configuration cfg, int barCount) {
-		super(cfg, barCount);
+		super(cfg);
+		sample = new RingArray<>(barCount);
 	}
 
 	@Override
@@ -17,10 +20,14 @@ public class MAIndicator extends AbstractMAIndicator implements Indicator {
 	}
 
 	@Override
-	protected Num evaluate(Num num) {
+	public void update(Num num) {
+		super.update(evaluate(num));
+	}
+
+	private Num evaluate(Num num) {
 		sample.update(num, num.unstable()).ifPresent(val -> sum -= val.value());	// 减去旧的值
 		sum += num.value();															// 加上新的值
 		return Num.of(sum / sample.size(), num.unstable());
 	}
-
+	
 }
