751366aa0bc9256d00b68af8e55567b7821ab043
KevinHuangwl <kevinbauer@163.com>
2023-05-07 17:23:40 +0800

    修复标准差指标没有实现对其引用的基础指标进行运算的问题

--- northstar-api/src/main/java/org/dromara/northstar/indicator/helper/StandardDeviationIndicator.java
+++ northstar-api/src/main/java/org/dromara/northstar/indicator/helper/StandardDeviationIndicator.java
@@ -23,23 +23,42 @@ public class StandardDeviationIndicator extends AbstractIndicator implements Ind
 	
 	private RingArray<Num> sample;
 	
+	/**
+	 * 标准差指标
+	 * @param cfg
+	 * @param barCount		样本大小
+	 */
 	public StandardDeviationIndicator(Configuration cfg, int barCount) {
 		super(cfg);
 		this.sample = new RingArray<>(barCount);
 	}
 	
-	public StandardDeviationIndicator(Configuration cfg, Indicator indicator, int barCount) {
-		this(cfg, barCount);
+	/**
+	 * 标准差指标
+	 * @param cfg
+	 * @param indicator		样本指标（指标的可回溯长度就是样本大小）
+	 */
+	public StandardDeviationIndicator(Configuration cfg, Indicator indicator) {
+		this(cfg, 0);
 		this.srcIndicator = indicator;
 	}
 	
 	@Override
 	protected Num evaluate(Num num) {
-		sample.update(num, num.unstable());
-		if(sample.toArray().length != sample.size()) {
-			return Num.NaN();
+		double[] data = null;
+		if(Objects.isNull(srcIndicator)) {
+			sample.update(num, num.unstable());
+			if(sample.toArray().length != sample.size()) {
+				return Num.NaN();
+			}
+			data = Stream.of(sample.toArray()).map(Num.class::cast).mapToDouble(Num::value).toArray();
+		} else {
+			if(!srcIndicator.isReady()) {
+				return Num.NaN();
+			}
+			data = srcIndicator.getData().stream().mapToDouble(Num::value).toArray();
 		}
-		double[] data = Stream.of(sample.toArray()).map(Num.class::cast).mapToDouble(Num::value).toArray();
+		
 		double std = new StandardDeviation().evaluate(data);
 		return Num.of(std, num.timestamp(), num.unstable());
 	}
