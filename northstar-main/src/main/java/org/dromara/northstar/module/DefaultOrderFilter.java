b853e639c12f1e7e7fcd889d2eaf70793698fa91
KevinHuangwl <kevinbauer@163.com>
2023-05-16 20:15:30 +0800

    优化日志输出

--- northstar-main/src/main/java/org/dromara/northstar/module/DefaultOrderFilter.java
+++ northstar-main/src/main/java/org/dromara/northstar/module/DefaultOrderFilter.java
@@ -39,18 +39,19 @@ public class DefaultOrderFilter implements OrderRequestFilter {
 	
 	private void prepareCounters() {
 		contractReqCounterMap.clear();
-		module.getModuleDescription().getModuleAccountSettingsDescription().stream().flatMap(mard -> mard.getBindedContracts().stream())
+		module.getModuleDescription().getModuleAccountSettingsDescription().stream().flatMap(mad -> mad.getBindedContracts().stream())
 			.forEach(contract -> contractReqCounterMap.put(contract.getUnifiedSymbol(), new AtomicInteger()));
 	}
 
 	@Override
 	public void doFilter(SubmitOrderReqField orderReq) {
+		module.getModuleContext().getLogger().info("默认订单过滤器正进行风控过滤");
 		if(!contractReqCounterMap.containsKey(orderReq.getContract().getUnifiedSymbol())) {
 			throw new NoSuchElementException(String.format("模组没包含合约：%s。 可选合约：%s", orderReq.getContract().getUnifiedSymbol(), contractReqCounterMap.keySet()));
 		}
 		String unifiedSymbol = orderReq.getContract().getUnifiedSymbol();
 		if(module.getModuleContext().getLogger().isDebugEnabled()) {
-			module.getModuleContext().getLogger().debug("默认订单过滤器正进行风控过滤，[{}] 当天 [{}] 合约的剩余发单次数为：{}", tradingDay, unifiedSymbol, contractReqCounterMap.get(unifiedSymbol).get());
+			module.getModuleContext().getLogger().debug("[{}] 当天 [{}] 合约的剩余发单次数为：{}", tradingDay, unifiedSymbol, contractReqCounterMap.get(unifiedSymbol).get());
 		}
 		if(contractReqCounterMap.get(unifiedSymbol).getAndIncrement() > MAX_ORDER_REQ_PER_DAY) {
 			module.getModuleContext().getLogger().warn("模组 [{}] 触发 [{}] 合约的日内免费申报上限。自动停用模组。", module.getName(), orderReq.getContract().getName());
