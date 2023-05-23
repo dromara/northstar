8d66399ec651fd23bd678897223bddefdd4bee2a
KevinHuangwl <kevinbauer@163.com>
2023-05-23 09:17:02 +0800

    修复加载顺序无指定导致的异常问题

--- northstar-gateway-ctp/src/main/java/org/dromara/northstar/gateway/ctp/CtpLoader.java
+++ northstar-gateway-ctp/src/main/java/org/dromara/northstar/gateway/ctp/CtpLoader.java
@@ -11,6 +11,7 @@ import org.dromara.northstar.gateway.GatewayMetaProvider;
 import org.dromara.northstar.gateway.IMarketCenter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,7 @@ import xyz.redtorch.gateway.ctp.x64v6v3v15v.CtpGatewayFactory;
 import xyz.redtorch.gateway.ctp.x64v6v5v1cpv.CtpSimGatewayFactory;
 
 @Slf4j
+@Order(0)	// 加载顺序需要显式声明，否则会最后才被加载，从而导致加载网关与模组时报异常
 @Component
 public class CtpLoader implements CommandLineRunner{
 	
