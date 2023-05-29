a5e49af535f3c9b2327fa44ab0f4683c62f9eb29
KevinHuangwl <kevinbauer@163.com>
2023-05-30 07:46:05 +0800

    修复测试邮件功能设计不合理的问题

--- northstar-main/src/main/java/org/dromara/northstar/web/restful/EmailConfigController.java
+++ northstar-main/src/main/java/org/dromara/northstar/web/restful/EmailConfigController.java
@@ -1,8 +1,6 @@
 package org.dromara.northstar.web.restful;
 
 import org.apache.commons.lang3.StringUtils;
-import org.dromara.northstar.common.event.NorthstarEvent;
-import org.dromara.northstar.common.event.NorthstarEventType;
 import org.dromara.northstar.common.model.MailConfigDescription;
 import org.dromara.northstar.common.model.ResultBean;
 import org.dromara.northstar.data.IMailConfigRepository;
@@ -15,9 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import xyz.redtorch.pb.CoreEnum.CommonStatusEnum;
-import xyz.redtorch.pb.CoreField.NoticeField;
 
 /**
  * @author KevinHuangwl
@@ -54,11 +49,7 @@ public class EmailConfigController implements InitializingBean{
 	
 	@GetMapping("test")
 	public void testMail() {
-		mailMgr.onEvent(new NorthstarEvent(NorthstarEventType.NOTICE, NoticeField.newBuilder()
-				.setStatus(CommonStatusEnum.COMS_INFO)
-				.setContent("邮件测试")
-				.setTimestamp(System.currentTimeMillis())
-				.build()));
+		mailMgr.sendTestMail();
 	}
 
 	@Override
