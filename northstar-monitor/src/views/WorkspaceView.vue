4a09ba9f7cb459426fe411d83ebd761f672395de
KevinHuangwl <kevinbauer@163.com>
2023-07-21 21:45:54 +0800

    更新监控台导航页

--- northstar-monitor/src/views/WorkspaceView.vue
+++ northstar-monitor/src/views/WorkspaceView.vue
@@ -160,7 +160,9 @@ export default {
 /* 移动端样式 */
 @media screen and (max-width: 660px) {
   .ns-tools button,
-  .ns-menu li:not(:nth-child(3))
+  .ns-menu li:nth-child(1),
+  .ns-menu li:nth-child(2),
+  .ns-menu li:nth-child(5)
   {
     display: none;
   }
