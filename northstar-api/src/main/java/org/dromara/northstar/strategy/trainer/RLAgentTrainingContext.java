74819a2bf37ce737e2999da7565ef641c4ecafd3
KevinHuangwl <kevinbauer@163.com>
2023-09-02 20:54:24 +0800

    更新接口默认实现

--- northstar-api/src/main/java/org/dromara/northstar/strategy/trainer/RLAgentTrainingContext.java
+++ northstar-api/src/main/java/org/dromara/northstar/strategy/trainer/RLAgentTrainingContext.java
@@ -2,16 +2,36 @@ package org.dromara.northstar.strategy.trainer;
 
 import java.time.LocalDate;
 
+import org.dromara.northstar.common.model.ModuleAccountRuntimeDescription;
 import org.dromara.northstar.common.model.ModuleRuntimeDescription;
+import org.dromara.northstar.common.utils.CommonUtils;
 import org.dromara.northstar.strategy.tester.ModuleTesterContext;
+
+import cn.hutool.core.lang.Assert;
 
 public interface RLAgentTrainingContext extends ModuleTesterContext {
 
+	/**
+	 * 最大训练回合数
+	 * @return
+	 */
 	default int maxTrainingEpisodes() {
 		return 1000;
 	}
 	
-	boolean hasConverged(ModuleRuntimeDescription d1, ModuleRuntimeDescription d2);
+	/**
+	 * 模组绩效是否收敛
+	 * @param mrd1
+	 * @param mrd2
+	 * @return
+	 */
+	default boolean hasPerformanceConverged(ModuleRuntimeDescription mrd1, ModuleRuntimeDescription mrd2) {
+		Assert.isTrue(mrd1.getModuleName().equals(mrd2.getModuleName()), "两模组运行时对象不一致");
+		ModuleAccountRuntimeDescription mard1 = mrd1.getAccountRuntimeDescription();
+		ModuleAccountRuntimeDescription mard2 = mrd2.getAccountRuntimeDescription();
+		return CommonUtils.isEquals(mard1.getAccCloseProfit(), mard2.getAccCloseProfit()) 
+				&& mard1.getAccDealVolume() == mard2.getAccDealVolume();
+	}
 	
 	/**
 	 * 预热起始日
