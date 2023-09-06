c0e918b43b2090f4bdee549ecbc1871e817dc873
unknown <1754376900@qq.com>
2023-09-06 16:35:40 +0800

    增加RL推理代码

--- python-app/test1.py
+++
@@ -1,9 +0,0 @@
-class Test:
-    def __init__(self):
-        self.a = 1
-    
-    def hello(self):
-        while True:
-            if self.a == 2:
-                print("hi!")
-                break
--- python-app/test2.py
+++
@@ -1,9 +0,0 @@
-def haha():
-    while True:
-        if a == True:
-            print(1)
-            return
-        
-a = False
-haha()
-a = T
