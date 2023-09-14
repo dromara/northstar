57bd8ed6e0576c4441ffa960f28c6d518736e076
unknown <1754376900@qq.com>
2023-09-14 23:34:54 +0800

    fetch upstream from ai-module

--- python-app/agents/ppo/ppo.py
+++ python-app/agents/ppo/ppo.py
@@ -6,15 +6,6 @@ import torch
 import torch.nn as nn
 from torch.distributions import Categorical
 
-# set device to cpu or cuda
-device = torch.device('cpu')
-if(torch.cuda.is_available()): 
-    device = torch.device('cuda:0') 
-    torch.cuda.empty_cache()
-    print(f"Device set to : {str(torch.cuda.get_device_name(device))}")
-else:
-    print("Device set to : cpu")
-
 
 class RolloutBuffer:
     def __init__(self):
--- python-app/main.py
+++ python-app/main.py
@@ -1,3 +1,4 @@
+import torch
 from server import Server
 from flask import Flask, request, jsonify
 
@@ -14,3 +15,11 @@ def get_action():
     return server.get_action(data)
     
 app.run(port=5001)
+# set device to cpu or cuda
+device = torch.device('cpu')
+if(torch.cuda.is_available()): 
+    device = torch.device('cuda:0') 
+    torch.cuda.empty_cache()
+    print(f"Device set to : {str(torch.cuda.get_device_name(device))}")
+else:
+    print("Device set to : cpu")
