624c65c0648094b1559f5e5b9cfe06b7fe4c044d
unknown <1754376900@qq.com>
2023-09-19 10:38:05 +0800

    解耦RL策略相关内容到northstar-external-rl

--- northstar-api/src/main/java/org/dromara/northstar/strategy/trainer/RLAgentTrainingRunner.java
+++ northstar-api/src/main/java/org/dromara/northstar/strategy/trainer/RLAgentTrainingRunner.java
@@ -1,11 +1,5 @@
 package org.dromara.northstar.strategy.trainer;
 
-import org.dromara.northstar.common.IGatewayService;
-import org.dromara.northstar.common.IModuleService;
-import org.dromara.northstar.common.ObjectManager;
-import org.dromara.northstar.gateway.Gateway;
-import org.dromara.northstar.gateway.IContractManager;
-import org.dromara.northstar.strategy.IModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -20,15 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 @ConditionalOnProperty(prefix = "spring.profiles", name = "active", havingValue = "train")
 @Component
 public class RLAgentTrainingRunner implements CommandLineRunner{
-
-	private ObjectManager<Gateway> gatewayMgr = new GatewayManager();
-	private ObjectManager<IModule> moduleMgr;
-	private IContractManager contractMgr;
-	private IGatewayService gatewayService;
-	private IModuleService moduleService;
 	
 	@Autowired
-	private AbstractTrainer trainer = new RLTrainer(gatewayMgr, moduleMgr, contractMgr, gatewayService, moduleService);
+	private AbstractTrainer trainer;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,7 +24,7 @@ public class RLAgentTrainingRunner implements CommandLineRunner{
 			log.info("模组强化学习训练准备开始");
 			try {
 				Thread.sleep(5000);
-				// trainer.start();
+				trainer.start();
 			} catch (InterruptedException e) {
 				log.warn("", e);
 			}
--- northstar-strategy-example/src/main/java/params/isTrain.java
+++
@@ -1,5 +0,0 @@
-package params;
-
-public class isTrain {
-
-}
