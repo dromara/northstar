a36aa4d2072753192a9675a032e274475ba42dd8
KevinHuangwl <kevinbauer@163.com>
2023-09-28 09:20:32 +0800

    优化启动脚本

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 启动服务
-nohup java -Xlog:gc*:gc.log -Xmn2g -Xmx2g -Denv=prod -jar ~/northstar-dist/northstar*.jar >ns.log &
+nohup java -Xlog:gc*:gc.log -Xms2g -Xmx2g -Dloader.path=$(pwd) -Denv=prod -jar $(find /root/northstar-dist -name 'northstar-[0-9]*.*.jar') >/root/northstar-dist/ns.log &
