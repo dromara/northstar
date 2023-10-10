0ddda6ac9074c1929d5d151e4d13f3c1882cd3d3
KevinHuangwl <kevinbauer@163.com>
2023-10-10 23:33:04 +0800

    优化系统成交事件的消息过滤

--- northstar-main/src/main/java/org/dromara/northstar/event/EventNotificationHandler.java
+++ northstar-main/src/main/java/org/dromara/northstar/event/EventNotificationHandler.java
@@ -3,6 +3,7 @@ package org.dromara.northstar.event;
 import java.util.Objects;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.dromara.northstar.common.event.AbstractEventHandler;
 import org.dromara.northstar.common.event.GenericEventHandler;
 import org.dromara.northstar.common.event.NorthstarEvent;
@@ -15,8 +16,6 @@ import xyz.redtorch.pb.CoreField.TradeField;
 
 public class EventNotificationHandler extends AbstractEventHandler implements GenericEventHandler{
 	
-	private static final int ONE_MIN = 60000;
-
 	private IMessageSender sender;
 	
 	private Set<NorthstarEventType> subEvents;
@@ -34,7 +33,7 @@ public class EventNotificationHandler extends AbstractEventHandler implements Ge
 	@Override
 	protected void doHandle(NorthstarEvent e) {
 		if(Objects.isNull(sender) 
-				|| e.getData() instanceof TradeField trade && Math.abs(System.currentTimeMillis() - trade.getTradeTimestamp()) > ONE_MIN
+				|| e.getData() instanceof TradeField trade && StringUtils.isBlank(trade.getOriginOrderId())
 				|| e.getData() instanceof OrderField order && !OrderUtils.isValidOrder(order)) {
 			return;
 		}
