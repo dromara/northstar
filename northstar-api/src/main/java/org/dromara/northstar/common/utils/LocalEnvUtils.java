da0ff64b0c4bedb84fd0929e8e42ef314b983510
KevinHuangwl <kevinbauer@163.com>
2023-10-12 08:01:08 +0800

    兼容ubuntu获取MAC地址

--- northstar-api/src/main/java/org/dromara/northstar/common/utils/LocalEnvUtils.java
+++ northstar-api/src/main/java/org/dromara/northstar/common/utils/LocalEnvUtils.java
@@ -1,11 +1,15 @@
 package org.dromara.northstar.common.utils;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
+import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.env.Environment;
 
 import lombok.extern.slf4j.Slf4j;
@@ -25,17 +29,36 @@ public class LocalEnvUtils {
 	}
 	
 	public static String getMACAddress() {
+		String macAddr = "";
 		try {
 			byte[] hardwareAddress = getMACAddressBytes();
 			String[] hexadecimalFormat = new String[hardwareAddress.length];
 	        for (int i = 0; i < hardwareAddress.length; i++) {
 	            hexadecimalFormat[i] = String.format("%02X", hardwareAddress[i]);
 	        }
-	        return String.join("-", hexadecimalFormat);
+	        macAddr = String.join("-", hexadecimalFormat);
 		} catch (Exception e) {
-			log.warn("无法获取MAC地址", e);
+			log.warn("无法通过JAVA接口获取MAC地址", e);
+		}
+		
+		if(System.getProperties().getProperty("os.name").toUpperCase().indexOf("LINUX") != -1) {
+			try {
+				macAddr = getMACStringFromLinux();
+			} catch (IOException e) {
+				log.warn("无法通过Linux命令行获取MAC地址", e);
+			}
+		}
+		
+		return macAddr;
+	}
+	
+	private static String getMACStringFromLinux() throws IOException {
+		Process process = Runtime.getRuntime().exec("ifconfig | grep ether");
+		List<String> lines = IOUtils.readLines(process.getInputStream(), StandardCharsets.UTF_8);
+		if(lines.isEmpty()) {
 			return "";
 		}
+		return lines.get(0).trim().split("\\s+")[1];
 	}
 	
 	private static byte[] getMACAddressBytes() throws SocketException, UnknownHostException {
