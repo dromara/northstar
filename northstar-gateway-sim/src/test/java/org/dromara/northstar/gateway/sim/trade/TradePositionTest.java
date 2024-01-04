6f56a7a23081f37189ab290c563cd0c5f0620b9a
KevinHuangwl <kevinbauer@163.com>
2024-01-04 18:51:04 +0800

    更新单元测试

--- northstar-gateway-sim/src/test/java/org/dromara/northstar/gateway/sim/trade/TradePositionTest.java
+++ northstar-gateway-sim/src/test/java/org/dromara/northstar/gateway/sim/trade/TradePositionTest.java
@@ -18,8 +18,8 @@ import xyz.redtorch.pb.CoreEnum.OffsetFlagEnum;
 class TradePositionTest {
 	
 	LocalDate today = LocalDate.now();
-	Contract c1 = Contract.builder().symbol("rb2205@SHFE").multiplier(10).longMarginRatio(0.08).shortMarginRatio(0.08).build();
-	Contract c2 = Contract.builder().symbol("rb2210@SHFE").multiplier(10).longMarginRatio(0.08).shortMarginRatio(0.08).build();
+	Contract c1 = Contract.builder().unifiedSymbol("rb2205@SHFE").multiplier(10).longMarginRatio(0.08).shortMarginRatio(0.08).build();
+	Contract c2 = Contract.builder().unifiedSymbol("rb2210@SHFE").multiplier(10).longMarginRatio(0.08).shortMarginRatio(0.08).build();
 	
 	Trade openTrade = Trade.builder().tradingDay(today.minusDays(1)).contract(c1).price(5000).volume(2).direction(DirectionEnum.D_Buy).offsetFlag(OffsetFlagEnum.OF_Open).build();
 	Trade openTrade1 = Trade.builder().tradingDay(today).contract(c1).price(5300).volume(1).direction(DirectionEnum.D_Buy).offsetFlag(OffsetFlagEnum.OF_Open).build();
