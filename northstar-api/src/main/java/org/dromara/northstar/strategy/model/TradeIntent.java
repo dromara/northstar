d52a6b9f663355935f221e5980ba3ea6a6136160
KevinHuangwl <kevinbauer@163.com>
2024-03-27 23:27:50 +0800

    更新交易滑点统一的输出位置

--- northstar-api/src/main/java/org/dromara/northstar/strategy/model/TradeIntent.java
+++ northstar-api/src/main/java/org/dromara/northstar/strategy/model/TradeIntent.java
@@ -120,10 +120,6 @@ public class TradeIntent implements TransactionAware, TickDataAware {
 		}
 		if(hasTerminated()) {
 			logger.debug("交易意图已终止");
-			if(!trades.isEmpty()) {
-				double avgDealPrice = trades.stream().mapToDouble(tr -> tr.price() * tr.volume()).sum() / volume;
-				logger.info("交易滑点为：[{}]个价位", (int)(Math.abs(avgDealPrice - price) / contract.priceTick()));
-			}
 			return;
 		}
 		if(orderIdRef.isEmpty() && !context.getState().isOrdering()) {
@@ -163,6 +159,10 @@ public class TradeIntent implements TransactionAware, TickDataAware {
 			.ifPresent(id -> {
 				accVol += trade.volume();
 				trades.add(trade);
+				if(hasTerminated()) {
+					double avgDealPrice = trades.stream().mapToDouble(tr -> tr.price() * tr.volume()).sum() / volume;
+					logger.info("交易滑点为：[{}]个价位", (int)(Math.abs(avgDealPrice - price) / contract.priceTick()));
+				}
 			});
 	}
 
