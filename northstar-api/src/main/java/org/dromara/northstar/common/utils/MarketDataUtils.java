823a6ac5a89ad3e7adc822532d2fc63948503326
KevinHuangwl <kevinbauer@163.com>
2024-04-13 12:50:10 +0800

    增加行情工具类

---
+++ northstar-api/src/main/java/org/dromara/northstar/common/utils/MarketDataUtils.java
@@ -0,0 +1,77 @@
+package org.dromara.northstar.common.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import org.dromara.northstar.common.model.core.Bar;
+import org.dromara.northstar.common.model.core.Tick;
+import org.dromara.northstar.common.model.core.TimeSlot;
+import org.dromara.northstar.common.model.core.TradeTimeDefinition;
+
+public class MarketDataUtils {
+
+	MarketDataUtils(){}
+	
+	/**
+	 * 判断Bar是否为开盘K线
+	 * @param bar
+	 * @return
+	 */
+	public static boolean isOpenningBar(Bar bar) {
+		TradeTimeDefinition ttd = bar.contract().contractDefinition().tradeTimeDef();
+		return ttd.timeSlots().get(0).start().equals(bar.actionTime());
+	}
+	
+	/**
+	 * 判断Bar是否为小节收盘K线
+	 * @param bar
+	 * @return
+	 */
+	public static boolean isEndingBar(Bar bar) {
+		TradeTimeDefinition ttd = bar.contract().contractDefinition().tradeTimeDef();
+		for(TimeSlot ts : ttd.timeSlots()) {
+			if(ts.end().equals(bar.actionTime())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 判断TICK是否为开盘范围内的TICK
+	 * 比如要判断一个tick是否为开盘10秒以内的tick，则调用 isOpenningTick(tick, 10000) 来进行判断
+	 * @param tick
+	 * @param seconds	开盘范围
+	 * @return
+	 */
+	public static boolean isOpenningTick(Tick tick, int seconds) {
+		TradeTimeDefinition ttd = tick.contract().contractDefinition().tradeTimeDef();
+		LocalTime openTime = ttd.timeSlots().get(0).start();
+		LocalTime cutoffTime = openTime.plusSeconds(seconds);
+		return tick.actionTime().isBefore(cutoffTime);
+	}
+	
+	/**
+	 * 距离整分钟还剩余几秒
+	 * @param tick
+	 * @return
+	 */
+	public static int secondsToWholeMin(Tick tick) {
+		long t = tick.actionTimestamp();
+		return (int) (60 - ((t % 60000) / 1000));
+	}
+	
+	/**
+	 * 距离日内收盘还剩余几分钟
+	 * @param bar
+	 * @return
+	 */
+	public static int minutesToDayEnd(Bar bar) {
+		TradeTimeDefinition ttd = bar.contract().contractDefinition().tradeTimeDef();
+		LocalDate tradingDay = bar.tradingDay();
+		LocalTime endTime = ttd.timeSlots().get(ttd.timeSlots().size() - 1).end();
+		LocalDateTime endDateTime = LocalDateTime.of(tradingDay, endTime);
+		return (int) (CommonUtils.localDateTimeToMills(endDateTime) - bar.actionTimestamp()) / 60000;
+	}
+}
---
+++ northstar-api/src/test/java/org/dromara/northstar/common/utils/MarketDataUtilsTest.java
@@ -0,0 +1,120 @@
+package org.dromara.northstar.common.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.dromara.northstar.common.model.core.Bar;
+import org.dromara.northstar.common.model.core.Contract;
+import org.dromara.northstar.common.model.core.ContractDefinition;
+import org.dromara.northstar.common.model.core.Tick;
+import org.dromara.northstar.common.model.core.TimeSlot;
+import org.dromara.northstar.common.model.core.TradeTimeDefinition;
+import org.junit.jupiter.api.Test;
+
+class MarketDataUtilsTest {
+	
+	TradeTimeDefinition ttd = TradeTimeDefinition.builder()
+			.timeSlots(List.of(
+						TimeSlot.builder().start(LocalTime.of(21, 0)).end(LocalTime.of(23, 0)).build(),
+						TimeSlot.builder().start(LocalTime.of(9, 0)).end(LocalTime.of(15, 0)).build()
+					))
+			.build();
+	
+	Contract c = Contract.builder()
+			.contractDefinition(ContractDefinition.builder()
+					.tradeTimeDef(ttd)
+					.build())
+			.build();
+
+	@Test
+	void testIsOpenningBar() {
+		LocalTime t1 = LocalTime.of(21, 0);
+		LocalTime t2 = LocalTime.of(9, 0);
+		
+		Bar b1 = Bar.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(t1)
+				.contract(c)
+				.build();
+		Bar b2 = Bar.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(t2)
+				.contract(c)
+				.build();
+		
+		assertThat(MarketDataUtils.isOpenningBar(b1)).isTrue();
+		assertThat(MarketDataUtils.isOpenningBar(b2)).isFalse();
+	}
+
+	@Test
+	void testIsEndingBar() {
+		LocalTime t1 = LocalTime.of(23, 0);
+		LocalTime t2 = LocalTime.of(15, 0);
+		LocalTime t3 = LocalTime.of(14, 59);
+		
+		Bar b1 = Bar.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(t1)
+				.contract(c)
+				.build();
+		Bar b2 = Bar.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(t2)
+				.contract(c)
+				.build();
+		Bar b3 = Bar.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(t3)
+				.contract(c)
+				.build();
+		
+		assertThat(MarketDataUtils.isEndingBar(b1)).isTrue();
+		assertThat(MarketDataUtils.isEndingBar(b2)).isTrue();
+		assertThat(MarketDataUtils.isEndingBar(b3)).isFalse();
+	}
+
+	@Test
+	void testIsOpenningTick() {
+		Tick t1 = Tick.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(LocalTime.of(21, 0, 5))
+				.actionTimestamp(CommonUtils.localDateTimeToMills(LocalDateTime.of(LocalDate.now(), LocalTime.of(21, 0, 5))))
+				.contract(c)
+				.build();
+		
+		assertThat(MarketDataUtils.isOpenningTick(t1, 6)).isTrue();
+		assertThat(MarketDataUtils.isOpenningTick(t1, 5)).isFalse();
+	}
+
+	@Test
+	void testSecondsToWholeMin() {
+		Tick t1 = Tick.builder()
+				.actionDay(LocalDate.now())
+				.actionTime(LocalTime.of(21, 0, 5))
+				.actionTimestamp(CommonUtils.localDateTimeToMills(LocalDateTime.of(LocalDate.now(), LocalTime.of(21, 0, 5))))
+				.contract(c)
+				.build();
+		
+		assertThat(MarketDataUtils.secondsToWholeMin(t1)).isEqualTo(55);
+	}
+
+	@Test
+	void testMinutesToDayEnd() {
+		LocalTime t1 = LocalTime.of(14, 59);
+		
+		Bar b1 = Bar.builder()
+				.tradingDay(LocalDate.now())
+				.actionDay(LocalDate.now())
+				.actionTime(t1)
+				.actionTimestamp(CommonUtils.localDateTimeToMills(LocalDateTime.of(LocalDate.now(), t1)))
+				.contract(c)
+				.build();
+		
+		assertThat(MarketDataUtils.minutesToDayEnd(b1)).isEqualTo(1);
+	}
+
+}
