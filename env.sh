414e63ca3fc7d62e45ed56c9b2a33749c7696b6b
KevinHuangwl <kevinbauer@163.com>
2024-09-17 16:10:28 +0800

    更新环境脚本

--- env.sh
+++ env.sh
@@ -7,17 +7,19 @@ yum install git wget -y
 
 mkdir -p ~/northstar-env ~/northstar-dist
 
+JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d. -f1)
+
 # 检查JDK环境
-if java -version 2>&1 | grep -q "java version 21"; 
+if [ "$JAVA_VERSION" -ne 21 ]; 
 then
-	echo "JDK21已安装"
-	java -version
-else
 	echo "安装JDK21"
-	cd ~/northstar-env && wget --no-check-certificate https://gitee.com/dromara/northstar/attach_files/1903632/download
+	cd ~/northstar-env && wget --no-check-certificate -o jdk-21_linux-x64_bin.tar.gz https://gitee.com/dromara/northstar/attach_files/1903632/download
 	tar -xvf jdk-21_linux-x64_bin.tar.gz
 	rm -f jdk-21_linux-x64_bin.tar.gz
 	ln -sf ~/northstar-env/$(find jdk* -maxdepth 0 -type d)/bin/* /usr/local/bin/
+else
+	echo "JDK21已安装"
+	java -version
 fi
 
 echo "环境安装完成"
