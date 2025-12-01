33212ad1bc8390ad0851688b86697afb09f5edaa
Huangwl <kevinbauer@163.com>
2025-12-01 09:13:47 +0000

    update env.sh.
    Signed-off-by: Huangwl <kevinbauer@163.com>

--- env.sh
+++ env.sh
@@ -3,7 +3,6 @@
 set -e
 
 echo "准备环境依赖..."
-yum install git wget -y
 
 mkdir -p ~/northstar-env ~/northstar-dist
 
@@ -13,7 +12,7 @@ JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | cut -d. 
 if [[ "$JAVA_VERSION" -ne 21 ]]; 
 then
 	echo "安装JDK21"
-	cd ~/northstar-env && wget --no-check-certificate -O jdk-21_linux-x64_bin.tar.gz https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
+	cd ~/northstar-env && curl -k -L -o jdk-21_linux-x64_bin.tar.gz https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.tar.gz
 	tar -xvf jdk-21_linux-x64_bin.tar.gz
 	rm -f jdk-21_linux-x64_bin.tar.gz
 	ln -sf ~/northstar-env/$(find jdk* -maxdepth 0 -type d)/bin/* /usr/local/bin/
